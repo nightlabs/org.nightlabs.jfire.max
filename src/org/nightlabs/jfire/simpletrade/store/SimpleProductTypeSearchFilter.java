6f7ee7d68cdf6bc824711c14c01b94b93648061f
marco <marco@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2006-03-06 19:50:42 +0000

    some bugfixes
    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireSimpleTrade@3426 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/simpletrade/store/SimpleProductTypeSearchFilter.java
+++ src/org/nightlabs/jfire/simpletrade/store/SimpleProductTypeSearchFilter.java
@@ -51,11 +51,9 @@ public class SimpleProductTypeSearchFilter extends ProductTypeSearchFilter {
 		return SimpleProductType.class;
 	}
 
-	/**
-	 * @see org.nightlabs.jdo.search.SearchFilter#prepareQuery(java.util.Set, java.lang.StringBuffer, java.lang.StringBuffer, java.lang.StringBuffer, java.util.Map, java.lang.StringBuffer)
-	 */
-	protected void prepareQuery(Set imports, StringBuffer vars,
-			StringBuffer filter, StringBuffer params, Map paramMap,
+	@Override
+	protected void prepareQuery(Set<Class> imports, StringBuffer vars,
+			StringBuffer filter, StringBuffer params, Map<String, Object> paramMap,
 			StringBuffer result) {
 		filter.append("this.published && this.saleable");
 	}

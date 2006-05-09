30fa246db2e7c04621d29849a6b6ebd6c2089ac5
alex <alex@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2006-05-09 13:53:28 +0000

    minor changes
    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireReporting@3802 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/reporting/ReportManagerBean.java
+++ src/org/nightlabs/jfire/reporting/ReportManagerBean.java
@@ -169,7 +169,8 @@ implements SessionBean
 			Collection configs = Config.getConfigsByType(pm, getOrganisationID(), UserConfigSetup.CONFIG_TYPE_USER_CONFIG);
 			for (Iterator iter = configs.iterator(); iter.hasNext();) {
 				Config config = (Config) iter.next();
-				ReportLayoutConfigModule configModule = (ReportLayoutConfigModule)ConfigModule.getAutoCreateConfigModule(pm, config, ReportLayoutConfigModule.class, null);
+				ReportLayoutConfigModule configModule = (ReportLayoutConfigModule)config.createConfigModule(ReportLayoutConfigModule.class, null);
+//				ReportLayoutConfigModule configModule = (ReportLayoutConfigModule)ConfigModule.getAutoCreateConfigModule(pm, config, ReportLayoutConfigModule.class, null);
 				configModule.getAvailEntry(catType).setDefaultReportLayoutKey(JDOHelper.getObjectId(layout).toString());
 				LOGGER.info("Set default for ReportLayoutConfigModule for category "+catType+" and Config "+config.getConfigKey());
 			}

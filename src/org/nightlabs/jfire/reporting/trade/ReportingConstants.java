ce5feea78c1669c5fa35373c188902463fd33a05
alex <alex@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2006-08-10 17:34:23 +0000

    added JFireReportingTrade to nightly build fixed internal layout accessing for the client
    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireReportingTrade@4348 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/reporting/trade/ReportingConstants.java
+++ src/org/nightlabs/jfire/reporting/trade/ReportingConstants.java
@@ -13,13 +13,13 @@ public class ReportingConstants {
 	public static final String REPORT_REGISTRY_ITEM_TYPE_GENERAL = "JFireReportingTrade-Reporting-General";
 	public static final String REPORT_REGISTRY_ITEM_TYPE_ORDER = "JFireReportingTrade-Reporting-Order";
 	public static final String REPORT_REGISTRY_ITEM_TYPE_OFFER = "JFireReportingTrade-Reporting-Offer";
-	public static final String REPORT_REGISTRY_ITEM_TYPE_INVOICEDATA = "JFireReportingTrade-Reporting-Invoice";
+	public static final String REPORT_REGISTRY_ITEM_TYPE_INVOICE = "JFireReportingTrade-Reporting-Invoice";
 	public static final String REPORT_REGISTRY_ITEM_TYPE_DELIVERY_NOTE = "JFireReportingTrade-Reporting-Delivery";
 
 	/**
 	 * Used as fallback value for script types where none was specified in the content.xml (of the root element).
 	 */
-	public static final String REPORT_REGISTRY_ITEM_TYPE_TRADE_SCRIPT = "JFireReportingTrade-Scripting";
+	public static final String REPORT_REGISTRY_ITEM_TYPE_TRADE = "JFireReportingTrade-Reporting";
 
 	public static final String REPORT_REGISTRY_ITEM_ID_CATEGORY_ROOT = "JFireReportingTrade-Reporting-Root";
 

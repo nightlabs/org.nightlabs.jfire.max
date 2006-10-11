226e951bb008647150f5d97e64b68285e849fa24
marco <marco@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2006-10-11 00:26:39 +0000

    extended SecurityReflector.UserDescriptor a bit
    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireJbpm@4711 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/jbpm/JFireJbpmAuthenticationService.java
+++ src/org/nightlabs/jfire/jbpm/JFireJbpmAuthenticationService.java
@@ -6,9 +6,16 @@ import org.nightlabs.jfire.security.SecurityReflector;
 public class JFireJbpmAuthenticationService implements AuthenticationService {
 	
 	private static final long serialVersionUID = 1L;
-	
+
 	public String getActorId() {
-		return SecurityReflector.getUserDescriptor().getUserID();
+		// TODO @KermitTheFragger: IMHO this should always be the complete user name with organisationID (not just userID).
+		// To make the code cleaner and life easier for you, I added the method getCompleteUserID() to UserDescriptor.
+		// You can delete this TO-DO note, once you've read it ;-) If you used getUserID() alone somewhere else, please change it there, too.
+		// And in case, you need to get a user from the actorId somewhere else, you can use code like this:
+		// SecurityReflector.UserDescriptor.parseLogin(actorId).getUser(pm); // where pm is your current PersistenceManager
+		// Marco :-)
+		// P.S.: Thanks for your contribution!!!
+		return SecurityReflector.getUserDescriptor().getCompleteUserID();
 	}
 
 	public void close() {

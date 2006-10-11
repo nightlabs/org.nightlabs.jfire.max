6fad1bde949702b9586256a853306a3b07b15b87
KermitTheFragger <KermitTheFragger@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2006-10-11 18:48:09 +0000

    Added JFire license headers to jBPM files added in previous commit
    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireJbpm@4715 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/jbpm/JFireJbpmAuthenticationService.java
+++ src/org/nightlabs/jfire/jbpm/JFireJbpmAuthenticationService.java
@@ -1,20 +1,43 @@
+/* *****************************************************************************
+ * JFire - it's hot - Free ERP System - http://jfire.org                       *
+ * Copyright (C) 2004-2005 NightLabs - http://NightLabs.org                    *
+ *                                                                             *
+ * This library is free software; you can redistribute it and/or               *
+ * modify it under the terms of the GNU Lesser General Public                  *
+ * License as published by the Free Software Foundation; either                *
+ * version 2.1 of the License, or (at your option) any later version.          *
+ *                                                                             *
+ * This library is distributed in the hope that it will be useful,             *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of              *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU           *
+ * Lesser General Public License for more details.                             *
+ *                                                                             *
+ * You should have received a copy of the GNU Lesser General Public            *
+ * License along with this library; if not, write to the                       *
+ *     Free Software Foundation, Inc.,                                         *
+ *     51 Franklin St, Fifth Floor,                                            *
+ *     Boston, MA  02110-1301  USA                                             *
+ *                                                                             *
+ * Or get it online :                                                          *
+ *     http://opensource.org/licenses/lgpl-license.php                         *
+ *                                                                             *
+ *                                                                             *
+ ******************************************************************************/
+
 package org.nightlabs.jfire.jbpm;
 
 import org.jbpm.security.AuthenticationService;
 import org.nightlabs.jfire.security.SecurityReflector;
 
+/**
+ * @author Jasper Siepkes
+ *
+ */
 public class JFireJbpmAuthenticationService implements AuthenticationService {
 	
 	private static final long serialVersionUID = 1L;
 
 	public String getActorId() {
-		// TODO @KermitTheFragger: IMHO this should always be the complete user name with organisationID (not just userID).
-		// To make the code cleaner and life easier for you, I added the method getCompleteUserID() to UserDescriptor.
-		// You can delete this TO-DO note, once you've read it ;-) If you used getUserID() alone somewhere else, please change it there, too.
-		// And in case, you need to get a user from the actorId somewhere else, you can use code like this:
-		// SecurityReflector.UserDescriptor.parseLogin(actorId).getUser(pm); // where pm is your current PersistenceManager
-		// Marco :-)
-		// P.S.: Thanks for your contribution!!!
 		return SecurityReflector.getUserDescriptor().getCompleteUserID();
 	}
 

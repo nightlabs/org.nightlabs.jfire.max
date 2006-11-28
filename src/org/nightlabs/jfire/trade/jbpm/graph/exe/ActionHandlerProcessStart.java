d7181efe2e5ac652d4266c6f61f316f8d889bd7c
marco <marco@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2006-11-28 05:27:21 +0000

    continued work on jbpm integration
    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireTrade@5109 18c61ebf-f106-0410-9e1e-81fb82415b8f

---
+++ src/org/nightlabs/jfire/trade/jbpm/graph/exe/ActionHandlerProcessStart.java
@@ -0,0 +1,16 @@
+package org.nightlabs.jfire.trade.jbpm.graph.exe;
+
+import org.jbpm.graph.def.ActionHandler;
+import org.jbpm.graph.exe.ExecutionContext;
+
+public class ActionHandlerProcessStart
+implements ActionHandler
+{
+	private static final long serialVersionUID = 1L;
+
+	public void execute(ExecutionContext executionContext)
+			throws Exception
+	{
+
+	}
+}

2a475093ac817fa78f6f41cf71c9e3fa1cd9240f
marco <marco@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2006-12-14 20:58:57 +0000

    - JFireServerManager: prevent organisation creation in early-server-inits - JFireChezFrancois: switching from early- to late-server-init - JFireTrade & JFireJbpm: continued work on jBPM integration - JFireTrade: implemented ReceptionNote and ReceptionNoteLocal
    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireJbpm@5222 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/jbpm/graph/def/State.java
+++ src/org/nightlabs/jfire/jbpm/graph/def/State.java
@@ -6,12 +6,10 @@ import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
 
-import javax.jdo.JDOHelper;
 import javax.jdo.PersistenceManager;
 import javax.jdo.Query;
 
 import org.nightlabs.jdo.ObjectID;
-import org.nightlabs.jfire.idgenerator.IDGenerator;
 import org.nightlabs.jfire.security.User;
 import org.nightlabs.jfire.trade.state.id.StateID;
 

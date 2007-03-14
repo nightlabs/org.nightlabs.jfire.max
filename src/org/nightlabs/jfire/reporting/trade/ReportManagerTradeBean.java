d708389bd6243d9a5f558fe26eb481de279615f3
alex <alex@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2007-03-14 16:27:09 +0000

    initialization for parameter acquisition test
    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireReportingTrade@5712 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/reporting/trade/ReportManagerTradeBean.java
+++ src/org/nightlabs/jfire/reporting/trade/ReportManagerTradeBean.java
@@ -27,18 +27,46 @@
 package org.nightlabs.jfire.reporting.trade;
 
 import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Set;
 
 import javax.ejb.CreateException;
 import javax.ejb.EJBException;
 import javax.ejb.SessionBean;
 import javax.ejb.SessionContext;
+import javax.jdo.JDOHelper;
+import javax.jdo.JDOObjectNotFoundException;
 import javax.jdo.PersistenceManager;
 
 import org.apache.log4j.Logger;
 import org.nightlabs.ModuleException;
+import org.nightlabs.jfire.accounting.id.InvoiceID;
 import org.nightlabs.jfire.base.BaseSessionBeanImpl;
+import org.nightlabs.jfire.idgenerator.IDGenerator;
 import org.nightlabs.jfire.organisation.Organisation;
+import org.nightlabs.jfire.reporting.layout.ReportLayout;
+import org.nightlabs.jfire.reporting.layout.id.ReportRegistryItemID;
+import org.nightlabs.jfire.reporting.parameter.ValueProvider;
+import org.nightlabs.jfire.reporting.parameter.ValueProviderCategory;
+import org.nightlabs.jfire.reporting.parameter.ValueProviderInputParameter;
+import org.nightlabs.jfire.reporting.parameter.config.AcquisitionParameterConfig;
+import org.nightlabs.jfire.reporting.parameter.config.ReportParameterAcquisitionSetup;
+import org.nightlabs.jfire.reporting.parameter.config.ReportParameterAcquisitionUseCase;
+import org.nightlabs.jfire.reporting.parameter.config.ValueAcquisitionSetup;
+import org.nightlabs.jfire.reporting.parameter.config.ValueConsumerBinding;
+import org.nightlabs.jfire.reporting.parameter.config.ValueProviderConfig;
+import org.nightlabs.jfire.reporting.parameter.config.ValueProviderProvider;
+import org.nightlabs.jfire.reporting.parameter.id.ValueProviderCategoryID;
+import org.nightlabs.jfire.reporting.parameter.id.ValueProviderID;
+import org.nightlabs.jfire.security.SecurityReflector;
 import org.nightlabs.jfire.servermanager.JFireServerManager;
+import org.nightlabs.jfire.store.id.ProductTypeID;
+import org.nightlabs.jfire.transfer.id.AnchorID;
 
 /**
  * Bean to initialize the default reports and report scripts.
@@ -56,7 +84,7 @@ public abstract class ReportManagerTradeBean
 extends BaseSessionBeanImpl
 implements SessionBean
 {
-	public static final Logger LOGGER = Logger.getLogger(ReportManagerTradeBean.class);
+	public static final Logger logger = Logger.getLogger(ReportManagerTradeBean.class);
 
 	/**
 	 * @see com.nightlabs.jfire.base.BaseSessionBeanImpl#setSessionContext(javax.ejb.SessionContext)
@@ -134,11 +162,202 @@ implements SessionBean
 			
 			ReportingInitializer.initialize(pm, jfireServerManager, Organisation.DEVIL_ORGANISATION_ID);
 			
+			initializeReportParameterAcquisition(pm);
+			
 		} finally {
 			pm.close();
 			jfireServerManager.close();
 		}
 		
 		
+	}
+	
+	private void initializeReportParameterAcquisition(final PersistenceManager pm) {
+		ValueProviderCategoryID categoryID = ValueProviderCategoryID.create(Organisation.DEVIL_ORGANISATION_ID, ReportingConstants.VALUE_PROVIDER_CATEGORY_ID_TRADE_REPORTS);
+		ValueProviderCategory rootCategory = createValueProviderCategory(pm, null, categoryID);
+		rootCategory.getName().setText(Locale.ENGLISH.getLanguage(), "Trade reports");
+		
+		ValueProviderCategoryID leCategoryID = ValueProviderCategoryID.create(Organisation.DEVIL_ORGANISATION_ID, ReportingConstants.VALUE_PROVIDER_CATEGORY_ID_LEGAL_ENTITY);
+		ValueProviderCategory leCategory = createValueProviderCategory(pm, rootCategory, leCategoryID);
+		leCategory.getName().setText(Locale.ENGLISH.getLanguage(), "Legal entity parameters");
+		
+		ValueProviderID leSearchID = ValueProviderID.create(
+				Organisation.DEVIL_ORGANISATION_ID, 
+				ReportingConstants.VALUE_PROVIDER_CATEGORY_ID_LEGAL_ENTITY, 
+				ReportingConstants.VALUE_PROVIDER_ID_LEGAL_ENTITY_SEARCH
+			);
+		ValueProvider leSearch = createValueProvider(pm, leCategory, leSearchID, AnchorID.class.getName());
+		leSearch.getName().setText(Locale.ENGLISH.getLanguage(), "Customer search");
+		leSearch.getDescription().setText(Locale.ENGLISH.getLanguage(), "Customer search");
+		
+		ValueProviderCategoryID docsCategoryID = ValueProviderCategoryID.create(Organisation.DEVIL_ORGANISATION_ID, ReportingConstants.VALUE_PROVIDER_CATEGORY_ID_TRADE_DOCUMENTS);
+		ValueProviderCategory docsCategory = createValueProviderCategory(pm, rootCategory, docsCategoryID);
+		docsCategory.getName().setText(Locale.ENGLISH.getLanguage(), "Trade documents parameters");
+		
+		ValueProviderCategoryID invoiceCategoryID = ValueProviderCategoryID.create(Organisation.DEVIL_ORGANISATION_ID, ReportingConstants.VALUE_PROVIDER_CATEGORY_ID_TRADE_DOCUMENTS_INVOICE);
+		ValueProviderCategory invoiceCategory = createValueProviderCategory(pm, rootCategory, invoiceCategoryID);
+		invoiceCategory.getName().setText(Locale.ENGLISH.getLanguage(), "Invoice parameters");
+		
+		ValueProviderID invoiceByCustomerID = ValueProviderID.create(
+				Organisation.DEVIL_ORGANISATION_ID, 
+				ReportingConstants.VALUE_PROVIDER_CATEGORY_ID_TRADE_DOCUMENTS_INVOICE, 
+				ReportingConstants.VALUE_PROVIDER_ID_TRADE_DOCUMENTS_INVOICE_BY_CUSTOMER
+			);
+		ValueProvider invoiceByCustomer = createValueProvider(pm, invoiceCategory, invoiceByCustomerID, InvoiceID.class.getName());
+		invoiceByCustomer.getName().setText(Locale.ENGLISH.getLanguage(), "Invoice of customer");
+		invoiceByCustomer.getDescription().setText(Locale.ENGLISH.getLanguage(), "Invoice of customer");
+		logger.debug("Creating parameters for "+invoiceByCustomerID);
+		invoiceByCustomer.getInputParameters().clear();
+		pm.flush();
+		invoiceByCustomer.addInputParameter(new ValueProviderInputParameter(invoiceByCustomer, "customer", AnchorID.class.getName()));
+		
+		
+		ValueProviderID invoiceByCustomerAndPeriodID = ValueProviderID.create(
+				Organisation.DEVIL_ORGANISATION_ID, 
+				ReportingConstants.VALUE_PROVIDER_CATEGORY_ID_TRADE_DOCUMENTS_INVOICE, 
+				ReportingConstants.VALUE_PROVIDER_ID_TRADE_DOCUMENTS_INVOICE_BY_CUSTOMER_AND_PERIOD
+			);
+		ValueProvider invoiceByCustomerAndPeriod = createValueProvider(pm, invoiceCategory, invoiceByCustomerAndPeriodID, InvoiceID.class.getName());
+		invoiceByCustomerAndPeriod.getName().setText(Locale.ENGLISH.getLanguage(), "Invoice of customer in period");
+		invoiceByCustomerAndPeriod.getDescription().setText(Locale.ENGLISH.getLanguage(), "Invoice of customer in period");
+		logger.debug("Creating parameters for "+invoiceByCustomerAndPeriodID);
+		invoiceByCustomerAndPeriod.getInputParameters().clear();
+		pm.flush();
+		invoiceByCustomerAndPeriod.addInputParameter(new ValueProviderInputParameter(invoiceByCustomer, "customer", AnchorID.class.getName()));
+		invoiceByCustomerAndPeriod.addInputParameter(new ValueProviderInputParameter(invoiceByCustomer, "from", Date.class.getName()));
+		invoiceByCustomerAndPeriod.addInputParameter(new ValueProviderInputParameter(invoiceByCustomer, "to", Date.class.getName()));
+		
+		
+		ValueProviderID invoiceByArticleTypeID = ValueProviderID.create(
+				Organisation.DEVIL_ORGANISATION_ID, 
+				ReportingConstants.VALUE_PROVIDER_CATEGORY_ID_TRADE_DOCUMENTS_INVOICE, 
+				ReportingConstants.VALUE_PROVIDER_ID_TRADE_DOCUMENTS_INVOICE_BY_ARTICLE_TYPE
+			);
+		ValueProvider invoiceByArticleType = createValueProvider(pm, invoiceCategory, invoiceByArticleTypeID, InvoiceID.class.getName());
+		invoiceByArticleType.getName().setText(Locale.ENGLISH.getLanguage(), "Invoice by article type");
+		invoiceByArticleType.getDescription().setText(Locale.ENGLISH.getLanguage(), "Invoice by article type");
+		logger.debug("Creating parameters for "+invoiceByArticleTypeID);
+		invoiceByArticleType.getInputParameters().clear();
+		pm.flush();
+		invoiceByArticleType.addInputParameter(new ValueProviderInputParameter(invoiceByCustomer, "productType", ProductTypeID.class.getName()));		
+		
+		
+		
+		// Configure report layout
+		
+		ReportRegistryItemID defInvoiceID = ReportRegistryItemID.create(
+				Organisation.DEVIL_ORGANISATION_ID, 
+				ReportingConstants.REPORT_REGISTRY_ITEM_TYPE_INVOICE, 
+				ReportingConstants.REPORT_REGISTRY_ITEM_ID_DEFAULT_INVOICE_LAYOUT
+			);
+		ReportLayout defInvoice = (ReportLayout) pm.getObjectById(defInvoiceID);
+		logger.debug("Have report layout");
+		
+		String organisationID = SecurityReflector.getUserDescriptor().getOrganisationID();
+		
+		ReportParameterAcquisitionSetup setup = ReportParameterAcquisitionSetup.getSetupForReportLayout(pm, (ReportRegistryItemID)JDOHelper.getObjectId(defInvoice));
+		if (setup != null) {
+			for (Map.Entry<ReportParameterAcquisitionUseCase, ValueAcquisitionSetup> entry : setup.getValueAcquisitionSetups().entrySet()) {
+//				pm.deletePersistent(entry.getKey());
+//				pm.deletePersistent(entry.getValue());
+				for (ValueConsumerBinding binding : entry.getValue().getValueConsumerBindings()) {
+					pm.deletePersistent(binding);
+				}				
+			}
+			setup.setDefaultSetup(null);
+//			pm.flush();
+//			setup.getValueAcquisitionSetups().clear();
+			pm.flush();
+			pm.deletePersistent(setup);
+			pm.flush();
+		}
+		setup = new ReportParameterAcquisitionSetup(organisationID, IDGenerator.nextID(ReportParameterAcquisitionSetup.class), defInvoice);
+		setup = (ReportParameterAcquisitionSetup) pm.makePersistent(setup);
+//		else  {
+//			setup.getValueAcquisitionSetups().clear();
+//			pm.flush();
+//		}
+		logger.debug("Created NEW setup");
+
+		ReportParameterAcquisitionUseCase defUseCase = new ReportParameterAcquisitionUseCase(setup, ReportParameterAcquisitionUseCase.USE_CASE_ID_DEFAULT);
+		defUseCase.getName().setText(Locale.ENGLISH.getLanguage(), "Default Usecase");
+		logger.debug("Created NEW default use case");
+		
+		ValueAcquisitionSetup valueAcquisitionSetup = new ValueAcquisitionSetup(organisationID, IDGenerator.nextID(ValueAcquisitionSetup.class), setup, defUseCase);
+		logger.debug("Created ValueAcquisitionSetup");
+		List<AcquisitionParameterConfig> parameterConfigs = new ArrayList<AcquisitionParameterConfig>();
+		AcquisitionParameterConfig pc1 = new AcquisitionParameterConfig(valueAcquisitionSetup);
+		pc1.setParameterID("invoiceID");
+		pc1.setParameterType(InvoiceID.class.getName());
+		parameterConfigs.add(pc1);
+		valueAcquisitionSetup.setParameterConfigs(parameterConfigs);
+		logger.debug("Created parameter config");
+		
+		Set<ValueProviderConfig> providerConfigs = new HashSet<ValueProviderConfig>();
+		ValueProviderConfig vpc1 = new ValueProviderConfig(valueAcquisitionSetup);
+		vpc1.setValueProvider(leSearch);
+		
+		ValueProviderConfig vpc2 = new ValueProviderConfig(valueAcquisitionSetup);
+		vpc2.setValueProvider(invoiceByCustomer);
+		
+		providerConfigs.add(vpc1);
+		providerConfigs.add(vpc2);
+		valueAcquisitionSetup.setValueProviderConfigs(providerConfigs);
+		logger.debug("Created value provider config");
+		
+		Set<ValueConsumerBinding> bindings = new HashSet<ValueConsumerBinding>();
+		ValueConsumerBinding b1 = new ValueConsumerBinding(organisationID, IDGenerator.nextID(ValueConsumerBinding.class), valueAcquisitionSetup);
+		b1.setConsumer(pc1);
+		b1.setParameterID("invoiceID");
+		b1.setProvider(vpc2);
+		
+		ValueConsumerBinding b2 = new ValueConsumerBinding(organisationID, IDGenerator.nextID(ValueConsumerBinding.class), valueAcquisitionSetup);
+		b2.setConsumer(vpc2);
+		b2.setParameterID("customer");
+		b2.setProvider(vpc1);
+		bindings.add(b1);
+		bindings.add(b2);
+		valueAcquisitionSetup.setValueConsumerBindings(bindings);
+		logger.debug("Created bindings");
+
+		setup.getValueAcquisitionSetups().put(defUseCase, valueAcquisitionSetup);
+		setup.setDefaultSetup(valueAcquisitionSetup);
+
+		ValueProviderProvider provider = new ValueProviderProvider() {
+			public ValueProvider getValueProvider(ValueProviderConfig valueProviderConfig) {
+				return (ValueProvider) pm.getObjectById(ValueProviderID.create(valueProviderConfig.getValueProviderOrganisationID(), valueProviderConfig.getValueProviderCategoryID(), valueProviderConfig.getValueProviderID()));
+			}
+		};
+		
+		setup.getDefaultSetup().createAcquisitionSequence(provider);
+		logger.debug("Persisted setup");
+	}
+	
+	private ValueProviderCategory createValueProviderCategory(PersistenceManager pm, ValueProviderCategory parent, ValueProviderCategoryID categoryID) {
+		ValueProviderCategory category = null;
+		try {
+			category = (ValueProviderCategory) pm.getObjectById(categoryID);
+			logger.debug("Have ValueProviderCategory "+categoryID);
+		} catch (JDOObjectNotFoundException e) {
+			logger.debug("Creating ValueProviderCategory "+categoryID);
+			category = new ValueProviderCategory(parent, categoryID.organisationID, categoryID.valueProviderCategoryID, true);
+			category = (ValueProviderCategory) pm.makePersistent(category);
+			logger.debug("Created ValueProviderCategory "+categoryID);
+		}
+		return category;
+	}
+	
+	private ValueProvider createValueProvider(PersistenceManager pm, ValueProviderCategory category, ValueProviderID valueProviderID, String outputType) {
+		ValueProvider valueProvider = null;
+		try {
+			valueProvider = (ValueProvider) pm.getObjectById(valueProviderID);
+			logger.debug("Have ValueProvider "+valueProviderID);
+		} catch (JDOObjectNotFoundException e) {
+			logger.debug("Creating ValueProvider "+valueProviderID);
+			valueProvider = new ValueProvider(category, valueProviderID.valueProviderID, outputType);
+			valueProvider = (ValueProvider) pm.makePersistent(valueProvider);
+			logger.debug("Created ValueProvider "+valueProviderID);
+		}
+		return valueProvider;
 	}
 }
--- src/org/nightlabs/jfire/reporting/trade/ReportingConstants.java
+++ src/org/nightlabs/jfire/reporting/trade/ReportingConstants.java
@@ -22,5 +22,24 @@ public class ReportingConstants {
 	public static final String REPORT_REGISTRY_ITEM_TYPE_TRADE = "JFireReportingTrade-Reporting";
 
 	public static final String REPORT_REGISTRY_ITEM_ID_CATEGORY_ROOT = "JFireReportingTrade-Reporting-Root";
+	public static final String REPORT_REGISTRY_ITEM_ID_CATEGORY_GENERAL = "General-Root";
+	public static final String REPORT_REGISTRY_ITEM_ID_CATEGORY_INVOICE = "Invoice-Root";
+	public static final String REPORT_REGISTRY_ITEM_ID_CATEGORY_ORDER = "Order-Root";
+	public static final String REPORT_REGISTRY_ITEM_ID_CATEGORY_OFFER = "Offer-Root";
+	public static final String REPORT_REGISTRY_ITEM_ID_CATEGORY_DELIVERY_NOTE = "DeliveryNote-Root";
+	
+	public static final String REPORT_REGISTRY_ITEM_ID_DEFAULT_INVOICE_LAYOUT = "Default-InvoiceLayout";
+	
+	
+	public static final String VALUE_PROVIDER_CATEGORY_ID_TRADE_REPORTS = "JFireReportingTrade-ValueProviderCategory-TradeReports";
 
+	public static final String VALUE_PROVIDER_CATEGORY_ID_LEGAL_ENTITY = "JFireReportingTrade-ValueProviderCategory-LegalEntity";
+	public static final String VALUE_PROVIDER_ID_LEGAL_ENTITY_SEARCH = "Search";
+
+	public static final String VALUE_PROVIDER_CATEGORY_ID_TRADE_DOCUMENTS = "JFireReportingTrade-ValueProviderCategory-TradeDocuments";
+	public static final String VALUE_PROVIDER_CATEGORY_ID_TRADE_DOCUMENTS_INVOICE = "JFireReportingTrade-ValueProviderCategory-TradeDocuments-Invoice";
+	public static final String VALUE_PROVIDER_ID_TRADE_DOCUMENTS_INVOICE_BY_CUSTOMER = "ByCustomer";
+	public static final String VALUE_PROVIDER_ID_TRADE_DOCUMENTS_INVOICE_BY_CUSTOMER_AND_PERIOD = "ByCustomerAndPeriod";
+	public static final String VALUE_PROVIDER_ID_TRADE_DOCUMENTS_INVOICE_BY_ARTICLE_TYPE = "ByArticleType";
+	
 }

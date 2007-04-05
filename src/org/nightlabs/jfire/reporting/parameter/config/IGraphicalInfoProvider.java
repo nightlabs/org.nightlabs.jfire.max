7520cb4554d69bfaf982f7e35623804540cd6e05
nozkiller <nozkiller@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2007-04-05 12:24:50 +0000

    IGraphicalInfoProvider
    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireReporting@6053 18c61ebf-f106-0410-9e1e-81fb82415b8f

---
+++ src/org/nightlabs/jfire/reporting/parameter/config/IGraphicalInfoProvider.java
@@ -0,0 +1,33 @@
+package org.nightlabs.jfire.reporting.parameter.config;
+
+/**
+ * @author Daniel.Mazurek [at] NightLabs [dot] de
+ *
+ */
+public interface IGraphicalInfoProvider 
+{
+	/**
+	 * returns the x coordinate
+	 * @return the x coordinate
+	 */
+	int getX();
+
+	/**
+	 * sets the x coordinate
+	 * @param x the x coordinate to set
+	 */
+	void setX(int x);
+	
+	/**
+	 * returns the y coordinate
+	 * @return the y coordinate
+	 */	
+	int getY();
+	
+	/**
+	 * sets the y coordinate
+	 * @param y the y coordinate to set
+	 */	
+	void setY(int y);
+	
+}

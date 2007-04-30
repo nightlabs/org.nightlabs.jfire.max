7dfd9812bfb78f2f834b758c99b43c909b97a12a
marco <marco@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2007-04-30 13:27:15 +0000

    fixed npe and added some javadoc
    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireReporting@6358 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/reporting/layout/render/RenderReportRequest.java
+++ src/org/nightlabs/jfire/reporting/layout/render/RenderReportRequest.java
@@ -4,6 +4,7 @@
 package org.nightlabs.jfire.reporting.layout.render;
 
 import java.io.Serializable;
+import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
 
@@ -30,69 +31,82 @@ public class RenderReportRequest implements Serializable {
 	private Map<String,Object> parameters;
 	
 	/**
-	 * 
+	 * This constructor must only be used in conjunction with ALL the setters (except for the locale) - otherwise the resulting object cannot be used.
 	 */
 	public RenderReportRequest() {
 	}
 
 	/**
-	 * 
+	 * @param reportRegistryItemID the id of the report which shall be rendered - must NOT be <code>null</code>.
+	 * @param parameters the parameters - can be <code>null</code> (<code>null</code> will be converted to an empty <code>Map</code>)
+	 * @param outputFormat the desired output-format - must NOT be <code>null</code>.
 	 */
 	public RenderReportRequest(
 			ReportRegistryItemID reportRegistryItemID,
 			Map<String, Object> parameters,
 			OutputFormat outputFormat
-		) 
+		)
 	{
-		this.reportRegistryItemID = reportRegistryItemID;
-		this.parameters = parameters;
-		this.outputFormat = outputFormat;
+		this.setReportRegistryItemID(reportRegistryItemID);
+		this.setParameters(parameters);
+		this.setOutputFormat(outputFormat);
 	}
 	
 	/**
-	 * @return the outputFormat
+	 * @return the outputFormat - should never be <code>null</code>
 	 */
 	public OutputFormat getOutputFormat() {
 		return outputFormat;
 	}
 
 	/**
-	 * @param outputFormat the outputFormat to set
+	 * @param outputFormat the desired output-format - must NOT be <code>null</code>.
 	 */
 	public void setOutputFormat(OutputFormat outputFormat) {
+		if (outputFormat == null)
+			throw new IllegalArgumentException("outputFormat must not be null!");
+
 		this.outputFormat = outputFormat;
 	}
 
 	/**
-	 * @return the parameters
+	 * @return the parameters - this is NEVER <code>null</code> - a new instance of {@link HashMap} is automatically assigned, if necessary.
 	 */
-	public Map<String, Object> getParameters() {
+	public Map<String, Object> getParameters()
+	{
+		if (parameters == null)
+			parameters = new HashMap<String, Object>(0);
+
 		return parameters;
 	}
 
 	/**
-	 * @param parameters the parameters to set
+	 * @param parameters the parameters - can be <code>null</code>
 	 */
 	public void setParameters(Map<String, Object> parameters) {
 		this.parameters = parameters;
 	}
 
 	/**
-	 * @return the reportRegistryItemID
+	 * @return the reportRegistryItemID - should never be <code>null</code>
 	 */
 	public ReportRegistryItemID getReportRegistryItemID() {
 		return reportRegistryItemID;
 	}
 
 	/**
-	 * @param reportRegistryItemID the reportRegistryItemID to set
+	 * @param reportRegistryItemID the id of the report which shall be rendered - must NOT be <code>null</code>.
 	 */
-	public void setReportRegistryItemID(ReportRegistryItemID reportRegistryItemID) {
+	public void setReportRegistryItemID(ReportRegistryItemID reportRegistryItemID)
+	{
+		if (reportRegistryItemID == null)
+			throw new IllegalArgumentException("reportRegistryItemID must not be null!");
+
 		this.reportRegistryItemID = reportRegistryItemID;
 	}
 
 	/**
-	 * @return The locale that should be used to render the report 
+	 * @return The locale that should be used to render the report - this can be <code>null</code>.
 	 */
 	public Locale getLocale() {
 		return locale;

57177bc59dabacd4075da4711a2b10ab7f9238ac
nozkiller <nozkiller@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2007-05-29 20:24:37 +0000

    DynamicProductTypeQuery
    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireDynamicTrade@6891 18c61ebf-f106-0410-9e1e-81fb82415b8f

---
+++ src/org/nightlabs/jfire/dynamictrade/store/search/DynamicProductTypeQuery.java
@@ -0,0 +1,32 @@
+package org.nightlabs.jfire.dynamictrade.store.search;
+
+import javax.jdo.Query;
+
+import org.nightlabs.jfire.dynamictrade.store.DynamicProductType;
+import org.nightlabs.jfire.store.search.ProductTypeQuery;
+
+/**
+ * @author Daniel.Mazurek [at] NightLabs [dot] de
+ *
+ */
+public class DynamicProductTypeQuery 
+extends ProductTypeQuery 
+{
+	public DynamicProductTypeQuery() {
+	}
+
+	@Override
+	protected Query prepareQuery() 
+	{
+		super.prepareQuery();
+		// FIXME: Query also subclasses when JPOX problem is solved
+		Query q = getPersistenceManager().newQuery(getPersistenceManager().getExtent(
+				DynamicProductType.class, false));
+		
+		q.setFilter(getFilter().toString());
+		q.declareVariables(getVars().toString());
+
+		return q;
+	}
+
+}

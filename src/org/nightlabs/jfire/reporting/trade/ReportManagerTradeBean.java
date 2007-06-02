74ea42c24500d9a3421e65dd2d92ce7c72c4d46d
alex <alex@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2007-06-02 17:42:58 +0000

    renamed initializer -> initialiser
    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireReportingTrade@6939 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/reporting/trade/ReportManagerTradeBean.java
+++ src/org/nightlabs/jfire/reporting/trade/ReportManagerTradeBean.java
@@ -37,7 +37,6 @@ import javax.ejb.SessionContext;
 import javax.jdo.PersistenceManager;
 
 import org.apache.log4j.Logger;
-import org.nightlabs.ModuleException;
 import org.nightlabs.jfire.base.BaseSessionBeanImpl;
 import org.nightlabs.jfire.organisation.Organisation;
 import org.nightlabs.jfire.reporting.ReportingInitialiserException;
@@ -47,6 +46,7 @@ import org.nightlabs.jfire.reporting.parameter.ValueProviderCategory;
 import org.nightlabs.jfire.reporting.parameter.ValueProviderInputParameter;
 import org.nightlabs.jfire.reporting.parameter.ReportParameterUtil.NameEntry;
 import org.nightlabs.jfire.reporting.parameter.id.ValueProviderCategoryID;
+import org.nightlabs.jfire.scripting.ScriptingIntialiserException;
 import org.nightlabs.jfire.servermanager.JFireServerManager;
 import org.nightlabs.jfire.store.id.ProductTypeID;
 import org.nightlabs.jfire.trade.id.ArticleContainerID;
@@ -103,21 +103,20 @@ implements SessionBean
 	/**
 	 * This method is called by the datastore initialization mechanism.
 	 * 
-	 * @throws ModuleException
-	 *
+	 * @throws ScriptingIntialiserException 
+	 * 
 	 * @ejb.interface-method
 	 * @ejb.permission role-name="_System_"
 	 * @ejb.transaction type = "Required"
 	 */
-	public void initializeScripting() 
-	throws ModuleException 
+	public void initializeScripting() throws ScriptingIntialiserException 
 	{
 		PersistenceManager pm;
 		pm = getPersistenceManager();
 		JFireServerManager jfireServerManager = getJFireServerManager();
 		try {
 			
-			ScriptingInitializer.initialize(pm, jfireServerManager, Organisation.DEVIL_ORGANISATION_ID);
+			ScriptingInitialiser.initialize(pm, jfireServerManager, Organisation.DEVIL_ORGANISATION_ID);
 			
 		} finally {
 			pm.close();
@@ -131,8 +130,6 @@ implements SessionBean
 	 * This method is called by the datastore initialization mechanism.
 	 * @throws ReportingInitializerException 
 	 * 
-	 * @throws ModuleException
-	 *
 	 * @ejb.interface-method
 	 * @ejb.permission role-name="JFireReporting-admin"
 	 * @ejb.transaction type = "Required"
@@ -222,95 +219,5 @@ implements SessionBean
 		pm.flush();
 		invoiceByArticleType.addInputParameter(new ValueProviderInputParameter(invoiceByCustomer, "productType", ProductTypeID.class.getName()));		
 		
-		
-		
-//		// Configure report layout
-//		
-//		ReportRegistryItemID defInvoiceID = ReportRegistryItemID.create(
-//				Organisation.DEVIL_ORGANISATION_ID, 
-//				ReportingTradeConstants.REPORT_REGISTRY_ITEM_TYPE_INVOICE, 
-//				ReportingTradeConstants.REPORT_REGISTRY_ITEM_ID_DEFAULT_INVOICE_LAYOUT
-//			);
-//		ReportLayout defInvoice = (ReportLayout) pm.getObjectById(defInvoiceID);
-//		logger.debug("Have report layout");
-//		
-//		String organisationID = SecurityReflector.getUserDescriptor().getOrganisationID();
-//		
-//		ReportParameterAcquisitionSetup setup = ReportParameterAcquisitionSetup.getSetupForReportLayout(pm, (ReportRegistryItemID)JDOHelper.getObjectId(defInvoice));
-//		if (setup != null) {
-//			for (Map.Entry<ReportParameterAcquisitionUseCase, ValueAcquisitionSetup> entry : setup.getValueAcquisitionSetups().entrySet()) {
-////				pm.deletePersistent(entry.getKey());
-////				pm.deletePersistent(entry.getValue());
-//				for (ValueConsumerBinding binding : entry.getValue().getValueConsumerBindings()) {
-//					pm.deletePersistent(binding);
-//				}				
-//			}
-//			setup.setDefaultSetup(null);
-////			pm.flush();
-////			setup.getValueAcquisitionSetups().clear();
-//			pm.flush();
-//			pm.deletePersistent(setup);
-//			pm.flush();
-//		}
-//		setup = new ReportParameterAcquisitionSetup(organisationID, IDGenerator.nextID(ReportParameterAcquisitionSetup.class), defInvoice);
-//		setup = (ReportParameterAcquisitionSetup) pm.makePersistent(setup);
-////		else  {
-////			setup.getValueAcquisitionSetups().clear();
-////			pm.flush();
-////		}
-//		logger.debug("Created NEW setup");
-//
-//		ReportParameterAcquisitionUseCase defUseCase = new ReportParameterAcquisitionUseCase(setup, ReportParameterAcquisitionUseCase.USE_CASE_ID_DEFAULT);
-//		defUseCase.getName().setText(Locale.ENGLISH.getLanguage(), "Default Usecase");
-//		logger.debug("Created NEW default use case");
-//		
-//		ValueAcquisitionSetup valueAcquisitionSetup = new ValueAcquisitionSetup(organisationID, IDGenerator.nextID(ValueAcquisitionSetup.class), setup, defUseCase);
-//		logger.debug("Created ValueAcquisitionSetup");
-//		List<AcquisitionParameterConfig> parameterConfigs = new ArrayList<AcquisitionParameterConfig>();
-//		AcquisitionParameterConfig pc1 = new AcquisitionParameterConfig(valueAcquisitionSetup);
-//		pc1.setParameterID("invoiceID");
-//		pc1.setParameterType(InvoiceID.class.getName());
-//		parameterConfigs.add(pc1);
-//		valueAcquisitionSetup.setParameterConfigs(parameterConfigs);
-//		logger.debug("Created parameter config");
-//		
-//		Set<ValueProviderConfig> providerConfigs = new HashSet<ValueProviderConfig>();
-//		ValueProviderConfig vpc1 = new ValueProviderConfig(valueAcquisitionSetup, IDGenerator.nextID(ValueProviderConfig.class));
-//		vpc1.setValueProvider(leSearch);
-//		
-//		ValueProviderConfig vpc2 = new ValueProviderConfig(valueAcquisitionSetup, IDGenerator.nextID(ValueProviderConfig.class));
-//		vpc2.setValueProvider(invoiceByCustomer);
-//		
-//		providerConfigs.add(vpc1);
-//		providerConfigs.add(vpc2);
-//		valueAcquisitionSetup.setValueProviderConfigs(providerConfigs);
-//		logger.debug("Created value provider config");
-//		
-//		Set<ValueConsumerBinding> bindings = new HashSet<ValueConsumerBinding>();
-//		ValueConsumerBinding b1 = new ValueConsumerBinding(organisationID, IDGenerator.nextID(ValueConsumerBinding.class), valueAcquisitionSetup);
-//		b1.setConsumer(pc1);
-//		b1.setParameterID("invoiceID");
-//		b1.setProvider(vpc2);
-//		
-//		ValueConsumerBinding b2 = new ValueConsumerBinding(organisationID, IDGenerator.nextID(ValueConsumerBinding.class), valueAcquisitionSetup);
-//		b2.setConsumer(vpc2);
-//		b2.setParameterID("customer");
-//		b2.setProvider(vpc1);
-//		bindings.add(b1);
-//		bindings.add(b2);
-//		valueAcquisitionSetup.setValueConsumerBindings(bindings);
-//		logger.debug("Created bindings");
-//
-//		setup.getValueAcquisitionSetups().put(defUseCase, valueAcquisitionSetup);
-//		setup.setDefaultSetup(valueAcquisitionSetup);
-//
-//		ValueProviderProvider provider = new ValueProviderProvider() {
-//			public ValueProvider getValueProvider(ValueProviderConfig valueProviderConfig) {
-//				return (ValueProvider) pm.getObjectById(ValueProviderID.create(valueProviderConfig.getValueProviderOrganisationID(), valueProviderConfig.getValueProviderCategoryID(), valueProviderConfig.getValueProviderID()));
-//			}
-//		};
-//		
-//		setup.getDefaultSetup().createAcquisitionSequence(provider);
-//		logger.debug("Persisted setup");
 	}
 }
--- src/org/nightlabs/jfire/reporting/trade/ScriptingInitialiser.java
+++ src/org/nightlabs/jfire/reporting/trade/ScriptingInitialiser.java
@@ -7,6 +7,7 @@ import javax.jdo.PersistenceManager;
 
 import org.nightlabs.ModuleException;
 import org.nightlabs.jfire.scripting.ScriptCategory;
+import org.nightlabs.jfire.scripting.ScriptingIntialiserException;
 import org.nightlabs.jfire.servermanager.JFireServerManager;
 
 /**
@@ -15,26 +16,26 @@ import org.nightlabs.jfire.servermanager.JFireServerManager;
  * @author Alexander Bieber <alex [AT] nightlabs [DOT] de>
  *
  */
-public class ScriptingInitializer {
+public class ScriptingInitialiser {
 	
 	/**
-	 * Uses a {@link org.nightlabs.jfire.scripting.ScriptingInitializer} to add
+	 * Uses a {@link org.nightlabs.jfire.scripting.ScriptingInitialiser} to add
 	 * all scripts in the script subdirectory of the JFireReportingTrade.ear.
 	 * It will create a root category for all scripts
 	 * 
 	 * @param pm The PersistenceManager to use.
 	 * @param jfireServerManager The ServerManager to use.
 	 * @param organisationID The organisationID the stored scirpts will have. 
+	 * @throws ScriptingIntialiserException 
 	 * @throws ModuleException
 	 */
 	public static void initialize(
 			PersistenceManager pm, 
 			JFireServerManager jfireServerManager,
 			String organisationID			
-		) 
-	throws ModuleException 
+		) throws ScriptingIntialiserException 
 	{
-		ScriptCategory rootCategory = org.nightlabs.jfire.scripting.ScriptingInitializer.createCategory(
+		ScriptCategory rootCategory = org.nightlabs.jfire.scripting.ScriptingInitialiser.createCategory(
 				pm, null, 
 				organisationID, 
 				ScriptingTradeConstants.SCRIPT_REGISTRY_ITEM_TYPE_ROOT, 
@@ -49,14 +50,14 @@ public class ScriptingInitializer {
 		File[] subDirs = scriptDir.listFiles();
 		for (int i = 0; i < subDirs.length; i++) {
 			if (subDirs[i].isDirectory()) {
-				new org.nightlabs.jfire.scripting.ScriptingInitializer(
+				new org.nightlabs.jfire.scripting.ScriptingInitialiser(
 						scriptDirSuffix+File.separator+subDirs[i].getName(),
 						rootCategory,
 						ScriptingTradeConstants.SCRIPT_REGISTRY_ITEM_TYPE_TRADE_SCRIPT,
 						jfireServerManager,
 						pm, 
 						organisationID
-					).initialize();
+					).initialise();
 			}
 		}
 	}

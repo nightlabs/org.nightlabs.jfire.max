6ec105e613aee52f27ec793a8ba5066ec26f0b04
marco <marco@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2007-06-05 18:01:25 +0000

    - renamed ShadowAccount to SummaryAccount - fixed a few bugs to get SummaryAccounts running in a cascaded way - reactivated and fixed UI for editing Account Configuration
    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireChezFrancois@6988 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/chezfrancois/DataCreator.java
+++ src/org/nightlabs/jfire/chezfrancois/DataCreator.java
@@ -251,7 +251,7 @@ public class DataCreator
 		Currency euro = getCurrencyEUR();
 
 		Account account = new Account(
-				organisationID, Account.ANCHOR_TYPE_ID_LOCAL_NORMAL, anchorID, organisationLegalEntity, euro);
+				organisationID, Account.ANCHOR_TYPE_ID_LOCAL_REVENUE_IN, anchorID, organisationLegalEntity, euro);
 		account.getName().setText(languageID, name);
 
 		pm.makePersistent(account);

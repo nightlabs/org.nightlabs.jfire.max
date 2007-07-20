1ed3ee0ed15185a82fd071304146ce3d067780c0
KermitTheFragger <KermitTheFragger@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2007-07-20 16:56:29 +0000

    Added JBPM logging module for JFire. Disabled by default; To enable remove commenting from jbpm.cfg.template.xml.
    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireJbpm@7853 18c61ebf-f106-0410-9e1e-81fb82415b8f

---
+++ src/org/nightlabs/jfire/jbpm/logging/JFireLoggingService.java
@@ -0,0 +1,90 @@
+/* JFire - it's hot - Free ERP System - http://jfire.org                       *
+ * Copyright (C) 2004-2005 NightLabs - http://NightLabs.org                    *
+ *                                                                             *
+ * This library is free software; you can redistribute it and/or               *
+ * modify it under the terms of the GNU Lesser General Public                  *
+ * License as published by the Free Software Foundation; either                *
+ * version 2.1 of the License, or (at your option) any later version.          *
+ *                                                                             *
+ * This library is distributed in the hope that it will be useful,             *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of              *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU           *
+ * Lesser General Public License for more details.                             *
+ *                                                                             *
+ * You should have received a copy of the GNU Lesser General Public            *
+ * License along with this library; if not, write to the                       *
+ *     Free Software Foundation, Inc.,                                         *
+ *     51 Franklin St, Fifth Floor,                                            *
+ *     Boston, MA  02110-1301  USA                                             *
+ *                                                                             *
+ * Or get it online :                                                          *
+ *     http://opensource.org/licenses/lgpl-license.php                         *
+ *                                                                             *
+ *                                                                             *
+ ******************************************************************************/
+
+package org.nightlabs.jfire.jbpm.logging;
+
+import java.util.Iterator;
+
+import javax.jdo.PersistenceManager;
+
+import org.apache.log4j.Logger;
+import org.jbpm.graph.log.TransitionLog;
+import org.jbpm.logging.LoggingService;
+import org.jbpm.logging.log.ProcessLog;
+import org.nightlabs.jfire.base.Lookup;
+import org.nightlabs.jfire.security.SecurityReflector;
+
+/**
+ * 
+ * This is a JFire specific class for Jbpm. It logs workflow
+ * transitions with JFire User, JFire organistation, Date, etc.
+ * 
+ * @author Jasper Siepkes <siepkes[AT]serviceplanet[DOT]nl>
+ */
+public class JFireLoggingService implements LoggingService {
+	
+	private static final Logger logger = Logger.getLogger(JFireLoggingService.class);
+	
+	private PersistenceManager pm;
+	
+	private String OrganisationID;
+	
+	public JFireLoggingService () {
+		logger.debug("Instantiated");
+		//Get the ID of the current organisation
+		this.OrganisationID = SecurityReflector.getUserDescriptor().getOrganisationID();
+		//Obtain a persistency manager for the current organisation
+		this.pm = new Lookup(OrganisationID).getPersistenceManager();		
+	}
+
+	public void log(ProcessLog processLog) {
+		if(processLog.getParent() != null) {
+			if (processLog.getParent().getChildren() != null) { 
+				logger.debug("Processing "+processLog.getParent().getChildren().size()+" workflow log(s)");
+				Iterator logIterator = processLog.getParent().getChildren().iterator();
+				
+				while (logIterator.hasNext()) {
+					Object log = logIterator.next();
+					if (log instanceof TransitionLog) { //We are only interested in workflow transitions.
+					TransitionLog transitionLog = (TransitionLog)log;
+					
+					pm.makePersistent(new LogEntry(OrganisationID, processLog.getDate(), 
+							transitionLog.getSourceNode().getFullyQualifiedName(), 
+							transitionLog.getDestinationNode().getFullyQualifiedName(),
+							transitionLog.getTransition().getName(),
+							processLog.getToken().getProcessInstance().getId(),
+							SecurityReflector.getUserDescriptor().getUser(pm)
+					));
+					}
+				}
+			}
+		}
+	}
+
+	public void close() {
+		//this.pm.close(); 
+		logger.debug("Closed");
+	}
+}
---
+++ src/org/nightlabs/jfire/jbpm/logging/LogEntry.java
@@ -0,0 +1,205 @@
+/* JFire - it's hot - Free ERP System - http://jfire.org                       *
+ * Copyright (C) 2004-2005 NightLabs - http://NightLabs.org                    *
+ *                                                                             *
+ * This library is free software; you can redistribute it and/or               *
+ * modify it under the terms of the GNU Lesser General Public                  *
+ * License as published by the Free Software Foundation; either                *
+ * version 2.1 of the License, or (at your option) any later version.          *
+ *                                                                             *
+ * This library is distributed in the hope that it will be useful,             *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of              *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU           *
+ * Lesser General Public License for more details.                             *
+ *                                                                             *
+ * You should have received a copy of the GNU Lesser General Public            *
+ * License along with this library; if not, write to the                       *
+ *     Free Software Foundation, Inc.,                                         *
+ *     51 Franklin St, Fifth Floor,                                            *
+ *     Boston, MA  02110-1301  USA                                             *
+ *                                                                             *
+ * Or get it online :                                                          *
+ *     http://opensource.org/licenses/lgpl-license.php                         *
+ *                                                                             *
+ *                                                                             *
+ ******************************************************************************/
+
+package org.nightlabs.jfire.jbpm.logging;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import org.nightlabs.jfire.idgenerator.IDGenerator;
+import org.nightlabs.jfire.security.User;
+
+/**
+ * @author Jasper Siepkes <siepkes[AT]serviceplanet[DOT]nl>
+ *
+ * @jdo.persistence-capable
+ *		identity-type = "application"
+ *		objectid-class = "org.nightlabs.jfire.jbpm.logging.id.logEntryID"
+ *		detachable = "true"
+ *		table = "JFireJbpm_Log"
+ *
+ * @jdo.inheritance strategy = "new-table"
+ *
+ * @jdo.create-objectid-class field-order = "organisationID, logEntryID"
+ */
+public class LogEntry implements Serializable {
+
+	////////BEGIN OF PRIMARY KEY(S) ////////////
+	/**
+	 * @jdo.field primary-key="true"
+	 * @jdo.column length="100"
+	 */
+	private String organisationID;
+	
+	/**
+	 * @jdo.field primary-key="true"
+	 */
+	private long logEntryID;
+	////////END OF PRIMARY KEY(S) //////////////
+	
+	/**
+	 * The date the workflow transition took place.
+	 * 
+	 * @jdo.field persistence-modifier="persistent"
+	 */
+	private Date date;
+	
+	/**
+	 * The name of the Transition from source to destination node.
+	 * 
+	 * @jdo.field persistence-modifier="persistent"
+	 */
+	private String transitionName;
+	
+	/**
+	 * The user who initiated the workflow transition.
+	 * 
+	 * @jdo.field persistence-modifier="persistent"
+	 */
+	private User user;
+	
+	/**
+	 * @jdo.field persistence-modifier="persistent"
+	 */
+	private long processInstanceID;
+	
+	/**
+	 * The node where the token was before the transition.
+	 * 
+	 * @jdo.field persistence-modifier="persistent"
+	 */
+	private String sourceNode;
+	
+	/**
+	 * The node where the token has traveld to.
+	 * 
+	 * @jdo.field persistence-modifier="persistent"
+	 */
+	private String destinationNode;
+
+	//////// BEGIN OF CONSTRUCTORS /////////////
+	/**
+	 * @deprecated This constructor is here because JDO requires it. Its not intended for any other purpose!
+	 */
+	protected LogEntry() { }
+	
+	/**
+	 * Default constructor
+	 * 
+	 * @param organisationID - The ID of the organisation in which the workflow transition takes place.
+	 * @param date - The Date the start of the workflow transition takes place.
+	 * @param sourceNode - The Workflow node from which the transition originates (Where the token started).
+	 * @param destinationNode - The Workflow node the transition leads to (Where the token is now). 
+	 * @param transitionName - The name of the transition itself.
+	 * @param processInstanceID - The ID of the instance of the workflow.
+	 * @param user - The User who initiated the transition.
+	 */
+	public LogEntry (String organisationID, 
+			Date date, 
+			String sourceNode, 
+			String destinationNode, 
+			String transitionName,
+			long processInstanceID,
+			User user) {
+		this.organisationID = organisationID;
+		this.logEntryID = createLogEntryID();
+		this.date = date;
+		this.sourceNode = sourceNode;
+		this.destinationNode = destinationNode;
+		this.transitionName = transitionName;
+		this.processInstanceID = processInstanceID;
+		this.user = user;
+	}
+
+	//////END OF CONSTRUCTORS /////////////////
+	
+	public static long createLogEntryID() {
+		return IDGenerator.nextID(LogEntry.class);
+	}
+	
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public String getDestinationNode() {
+		return destinationNode;
+	}
+
+	public void setDestinationNode(String destinationNode) {
+		this.destinationNode = destinationNode;
+	}
+
+	public long getLogEntryID() {
+		return logEntryID;
+	}
+
+	public void setLogEntryID(long logEntryID) {
+		this.logEntryID = logEntryID;
+	}
+
+	public String getOrganisationID() {
+		return organisationID;
+	}
+
+	public void setOrganisationID(String organisationID) {
+		this.organisationID = organisationID;
+	}
+
+	public long getProcessInstanceID() {
+		return processInstanceID;
+	}
+
+	public void setProcessInstanceID(long processID) {
+		this.processInstanceID = processID;
+	}
+
+	public String getSourceNode() {
+		return sourceNode;
+	}
+
+	public void setSourceNode(String sourceNode) {
+		this.sourceNode = sourceNode;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public void setTransitionName(String transitionName) {
+		this.transitionName = transitionName;
+	}
+
+	public String getTransitionName() {
+		return transitionName;
+	}	
+}

28374209082724eb41582bb2abec3024ee23250a
alex <alex@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2007-08-24 09:19:26 +0000

    removed test exception
    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireReporting@8232 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/reporting/oda/jfs/server/ServerJFSQueryProxy.java
+++ src/org/nightlabs/jfire/reporting/oda/jfs/server/ServerJFSQueryProxy.java
@@ -71,16 +71,15 @@ public class ServerJFSQueryProxy extends AbstractJFSQueryProxy {
 	public IParameterMetaData getParameterMetaData() 
 	throws OdaException 
 	{
-		throw new OdaException("This is a test exception");
-//		if (parameterMetaData == null) {
-//			ScriptRegistryItemID itemID = getScriptRegistryItemID();
-//			try {
-//				parameterMetaData = getScriptParameterMetaData(itemID);
-//			} catch (JFireReportingOdaException e) {
-//				throw new OdaException(e);
-//			}
-//		}
-//		return parameterMetaData;
+		if (parameterMetaData == null) {
+			ScriptRegistryItemID itemID = getScriptRegistryItemID();
+			try {
+				parameterMetaData = getScriptParameterMetaData(itemID);
+			} catch (JFireReportingOdaException e) {
+				throw new OdaException(e);
+			}
+		}
+		return parameterMetaData;
 	}
 	
 	/**

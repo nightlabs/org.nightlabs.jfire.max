8615ff9cfc142a039e43d2511e300f9b9e804f70
tobias <tobias@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2007-08-30 15:48:08 +0000

    Continued work on JFireTradeTestSuite.
    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireTrade@8311 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/transfer/Stage.java
+++ src/org/nightlabs/jfire/transfer/Stage.java
@@ -9,27 +9,39 @@ import java.util.EnumSet;
 public enum Stage {
 	Initial, ClientBegin, ServerBegin, ClientDoWork, ServerDoWork, ClientEnd, ServerEnd;
 	
-	public EnumSet<Stage> after() {
+	public EnumSet<Stage> after(boolean inclusive) {
 		EnumSet<Stage> after = EnumSet.noneOf(Stage.class);
 		boolean add = false;
 		for (Stage stage : values()) {
 			if (add)
-				after.add(this);
+				after.add(stage);
 			
-			if (stage == this)
+			if (stage == this) {
 				add = true;
+				
+				if (inclusive)
+					after.add(stage);
+			}
 		}
+		after.remove(Stage.Initial);
+		
 		return after;
 	}
 	
-	public EnumSet<Stage> before() {
+	public EnumSet<Stage> before(boolean inclusive) {
 		EnumSet<Stage> before = EnumSet.noneOf(Stage.class);
 		for (Stage stage : values()) {
-			if (stage == this)
+			if (stage == this) {
+				if (inclusive)
+					before.add(stage);
+				
 				break;
+			}
 			
-			before.add(this);
+			before.add(stage);
 		}
+		before.remove(Stage.Initial);
+		
 		return before;
 	}
 }

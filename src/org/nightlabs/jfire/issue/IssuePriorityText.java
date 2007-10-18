0796ad6940644ae106d2cf12cb6dad1b1d69db13
chairatk <chairatk@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2007-10-18 02:40:59 +0000

    - added i18nText classes.
    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireIssueTracking@8959 18c61ebf-f106-0410-9e1e-81fb82415b8f

---
+++ src/org/nightlabs/jfire/issue/IssuePriorityText.java
@@ -0,0 +1,84 @@
+package org.nightlabs.jfire.issue;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.nightlabs.i18n.I18nText;
+
+/**
+ * @author Chairat Kongarayawetchakun - chairat at nightlabs dot de
+ *
+ * @jdo.persistence-capable
+ *		identity-type="application"
+ *		objectid-class="org.nightlabs.jfire.issue.id.IssuePriorityTextID"
+ *		detachable="true"
+ *		table="JFireIssueTracking_IssuePriorityText"
+ *
+ * @jdo.inheritance strategy="new-table"
+ *
+ * @jdo.create-objectid-class
+ * 
+ * @jdo.fetch-group name="IssuePriorityText.text" fields="issue, texts"
+ */ 
+public class IssuePriorityText 
+extends I18nText{
+	/**
+	 * The serial version of this class.
+	 */
+	private static final long serialVersionUID = 1L;
+	/**
+	 * @jdo.field primary-key="true"
+	 * @jdo.column length="100"
+	 */
+	private String issueID;
+	
+	/**
+	 * @jdo.field persistence-modifier="persistent"
+	 */
+	private Issue issue;
+
+	/**
+	 * key: String languageID<br/>
+	 * value: String description
+	 * 
+	 * @jdo.field
+	 *		persistence-modifier="persistent"
+	 *		collection-type="map"
+	 *		key-type="java.lang.String"
+	 *		value-type="java.lang.String"
+	 *		table="JFireIssueTracking_IssuePriorityText_texts"
+	 *		null-value="exception"
+	 *
+	 * @jdo.join
+	 */
+	protected Map<String, String> texts = new HashMap<String, String>();
+
+	/**
+	 * @deprecated Only for JDO!
+	 */
+	protected IssuePriorityText()
+	{
+	}
+
+	public IssuePriorityText(Issue issue)
+	{
+		this.issue = issue;
+		issueID = issue.getIssueID();
+	}
+
+	/**
+	 * @see org.nightlabs.i18n.I18nText#getI18nMap()
+	 */
+	protected Map<String, String> getI18nMap()
+	{
+		return texts;
+	}
+
+	/**
+	 * @see org.nightlabs.i18n.I18nText#getFallBackValue(java.lang.String)
+	 */
+	protected String getFallBackValue(String languageID)
+	{
+		return issue == null ? languageID : issueID;
+	}
+}

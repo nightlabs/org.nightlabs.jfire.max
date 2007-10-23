b1061419293c6af756ede79dd47c04565faff1a1
chairatk <chairatk@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2007-10-23 13:28:20 +0000

    a new dao.
    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireIssueTracking@9023 18c61ebf-f106-0410-9e1e-81fb82415b8f

---
+++ src/org/nightlabs/jfire/issue/dao/IssueSeverityTypeDAO.java
@@ -0,0 +1,66 @@
+package org.nightlabs.jfire.issue.dao;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+
+import javax.jdo.FetchPlan;
+
+import org.nightlabs.jdo.NLJDOHelper;
+import org.nightlabs.jfire.base.jdo.BaseJDOObjectDAO;
+import org.nightlabs.jfire.issue.IssueManager;
+import org.nightlabs.jfire.issue.IssueManagerUtil;
+import org.nightlabs.jfire.issue.IssueSeverityType;
+import org.nightlabs.jfire.issue.id.IssueSeverityTypeID;
+import org.nightlabs.jfire.security.SecurityReflector;
+import org.nightlabs.progress.ProgressMonitor;
+
+public class IssueSeverityTypeDAO
+		extends BaseJDOObjectDAO<IssueSeverityTypeID, IssueSeverityType>
+{
+	private IssueSeverityTypeDAO() {}
+
+	private static IssueSeverityTypeDAO sharedInstance = null;
+
+	public static IssueSeverityTypeDAO sharedInstance() {
+		if (sharedInstance == null) {
+			synchronized (IssueSeverityTypeDAO.class) {
+				if (sharedInstance == null)
+					sharedInstance = new IssueSeverityTypeDAO();
+			}
+		}
+		return sharedInstance;
+	}
+	
+	@SuppressWarnings("unchecked") //$NON-NLS-1$
+	@Override
+	protected Collection<IssueSeverityType> retrieveJDOObjects(Set<IssueSeverityTypeID> objectIDs,
+			String[] fetchGroups, int maxFetchDepth, ProgressMonitor monitor)
+			throws Exception
+	{
+		monitor.beginTask("Fetching IssueSeverityTypes...", 1); //$NON-NLS-1$
+		try {
+			IssueManager im = IssueManagerUtil.getHome(SecurityReflector.getInitialContextProperties()).create();
+			monitor.worked(1);
+			return im.getIssueSeverityTypes(fetchGroups, maxFetchDepth);	
+		} catch (Exception e) {
+			monitor.setCanceled(true);
+			throw e;
+		} finally {
+			monitor.done();
+		}
+	}
+
+	private static final String[] FETCH_GROUPS = { FetchPlan.DEFAULT };
+
+	public List<IssueSeverityType> getIssueSeverityTypes(ProgressMonitor monitor)
+	{
+		try {
+			return new ArrayList<IssueSeverityType>(retrieveJDOObjects(null, FETCH_GROUPS, NLJDOHelper.MAX_FETCH_DEPTH_NO_LIMIT, monitor));
+		} catch (Exception e) {
+			throw new RuntimeException("Error while fetching issue severity types: " + e.getMessage(), e); //$NON-NLS-1$
+		} 
+	}
+
+}

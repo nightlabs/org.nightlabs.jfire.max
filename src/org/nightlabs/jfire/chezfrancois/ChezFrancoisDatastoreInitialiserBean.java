47d817e4699ec2d35e58073479e298fcd07f2451
marco <marco@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2007-12-04 22:12:41 +0000

    switched to next version number (mostly from 0.9.1 to 0.9.2)
    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireChezFrancois@9455 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/chezfrancois/ChezFrancoisDatastoreInitialiserBean.java
+++ src/org/nightlabs/jfire/chezfrancois/ChezFrancoisDatastoreInitialiserBean.java
@@ -154,7 +154,7 @@ implements SessionBean
 
 			// version is {major}.{minor}.{release}-{patchlevel}-{suffix}
 			moduleMetaData = new ModuleMetaData(
-					"JFireChezFrancois", "0.9.1-0-beta", "0.9.1-0-beta");
+					"JFireChezFrancois", "0.9.2-0-beta", "0.9.2-0-beta");
 			pm.makePersistent(moduleMetaData);
 
 

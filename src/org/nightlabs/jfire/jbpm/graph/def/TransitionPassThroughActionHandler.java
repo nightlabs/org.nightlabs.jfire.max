c169e0322758ade7959150bdcb7fee92f9eea71e
alex <alex@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2007-12-14 16:34:25 +0000

    - some restructuring in issue tracker ui - started jbpm ui for issue tracker - workflow for issue tracker
    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireJbpm@9572 18c61ebf-f106-0410-9e1e-81fb82415b8f

---
+++ src/org/nightlabs/jfire/jbpm/graph/def/TransitionPassThroughActionHandler.java
@@ -0,0 +1,65 @@
+/**
+ * 
+ */
+package org.nightlabs.jfire.jbpm.graph.def;
+
+import org.jbpm.graph.def.ActionHandler;
+import org.jbpm.graph.def.Node;
+import org.jbpm.graph.def.Transition;
+import org.jbpm.graph.exe.ExecutionContext;
+
+/**
+ * Simple {@link ActionHandler} for {@link Node}s that will leave the {@link Node}
+ * with a leaving {@link Transition} with the same name as the Node was entered.
+ * If that's not possible the Node will be left by the default Transition.
+ * <p>
+ * Note that this {@link ActionHandler} has to be added as action for the "node-enter" event
+ * as well as action for the node itself in order to function correctly.
+ * </p>
+ * @author Alexander Bieber <!-- alex [AT] nightlabs [DOT] de -->
+ */
+public class TransitionPassThroughActionHandler implements ActionHandler {
+
+	private static final long serialVersionUID = 1L;
+	
+	private static ThreadLocal<String> transitionThreadLocal = new ThreadLocal<String>();
+	
+	/**
+	 * 
+	 */
+	public TransitionPassThroughActionHandler() {
+	}
+
+	/* (non-Javadoc)
+	 * @see org.jbpm.graph.def.ActionHandler#execute(org.jbpm.graph.exe.ExecutionContext)
+	 */
+	public void execute(ExecutionContext ctx) throws Exception {
+		if (ctx.getTransition() != null) {
+			storeEnteringTransition(ctx.getTransition());
+		} else {
+			leaveNode(ctx);
+		}
+	}
+	
+	protected void storeEnteringTransition(Transition transition) {
+		transitionThreadLocal.set(transition.getName());
+	}
+	
+	protected String getEnteringTransitionName() {
+		return transitionThreadLocal.get();
+	}
+	
+	protected void leaveNode(ExecutionContext ctx) {
+		String transitionName = getEnteringTransitionName();
+		if (transitionName != null && !"".equals(transitionName)) {
+			Transition transition = ctx.getToken().getNode().getLeavingTransition(transitionName);
+			System.out.println("Context Transition: " + ctx.getTransition());
+			if (transition != null) {
+				ctx.leaveNode(transition);
+				return;
+			}				
+		}
+		ctx.leaveNode();
+	}
+
+}

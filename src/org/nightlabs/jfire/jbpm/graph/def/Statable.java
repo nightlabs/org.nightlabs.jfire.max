6aca146d2f87ee7cb60b19121579d991cb36a766
alex <alex@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2007-12-15 12:13:04 +0000

    continued a little on the IssueTracking
    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireJbpm@9579 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/jbpm/graph/def/Statable.java
+++ src/org/nightlabs/jfire/jbpm/graph/def/Statable.java
@@ -2,10 +2,36 @@ package org.nightlabs.jfire.jbpm.graph.def;
 
 import java.util.List;
 
+/**
+ * The {@link Statable} interface is used for Objects whose
+ * 'state' is managed by a Jbpm process.
+ * <p>
+ * Several objects in JFireJbpm operate on this interface.
+ * For example {@link ActionHandlerNodeEnter} which will 
+ * create a {@link State} whenever a node is entered and
+ * set it to the appropriate {@link #setState(State)}.
+ * </p>
+ * 
+ * @author Marco Schulze
+ */
 public interface Statable
 {
+	/**
+	 * @return The Local representation of this Statable where all states are stored.
+	 * 	The {@link Statable} itself usually stores only public states.  
+	 */
 	StatableLocal getStatableLocal();
+	/**
+	 * Set the current {@link State} of this {@link Statable} instance.
+	 * <p>
+	 * Implementations should also add the given state to the list of states returned in {@link #getStates()}. 
+	 * </p>
+	 * @param state The state to set.
+	 */
 	void setState(State state);
+	/**
+	 * @return The current {@link State} of this {@link Statable} instance.
+	 */
 	State getState();
 	/**
 	 * @return all {@link State} instances that are created for a public {@link StateDefinition} (see {@link StateDefinition#isPublicState()}) - no matter
@@ -13,6 +39,12 @@ public interface Statable
 	 */
 	List<State> getStates();
 
+	/**
+	 * Virtual fetch-group used by implementations that should include the current state. 
+	 */
 	static final String FETCH_GROUP_STATE = "Statable.state";
+	/**
+	 * Virtual fetch-group used by implementations that should include the current state. 
+	 */
 	static final String FETCH_GROUP_STATES = "Statable.states";
 }
--- src/org/nightlabs/jfire/jbpm/graph/def/StatableLocal.java
+++ src/org/nightlabs/jfire/jbpm/graph/def/StatableLocal.java
@@ -2,10 +2,34 @@ package org.nightlabs.jfire.jbpm.graph.def;
 
 import java.util.List;
 
+import org.jbpm.graph.exe.ProcessInstance;
+
+/**
+ * A {@link StatableLocal} should be assigned to a {@link Statable}
+ * it also holds the current status and a list of passed status but not only
+ * the public ones. Additionally this interface can store a reference to
+ * the Jbpm {@link ProcessInstance} that manages this {@link StatableLocal}
+ * and its {@link Statable}.
+ *  
+ * @author Marco Schulze
+ */
 public interface StatableLocal
 {
+	/**
+	 * @return The {@link Statable} this {@link StatableLocal} is linked to.
+	 */
 	Statable getStatable();
+	/**
+	 * Set the current {@link State} of this {@link StatableLocal}.
+	 * <p>
+	 * Implementations should also add the given state to the list of states returned in {@link #getStates()}. 
+	 * </p>
+	 * @param state The {@link State} to set.
+	 */
 	void setState(State state);
+	/**
+	 * @return The current {@link State} of this {@link StatableLocal}.
+	 */
 	State getState();
 
 	/**
@@ -13,9 +37,22 @@ public interface StatableLocal
 	 */
 	List<State> getStates();
 
+	/**
+	 * @return The id of {@link ProcessInstance} associated to this {@link StatableLocal}. 
+	 */
 	long getJbpmProcessInstanceId();
+	/**
+	 * Set the id of {@link ProcessInstance} associated to this {@link StatableLocal}.
+	 * @param jbpmProcessInstanceId The id to set.
+	 */
 	void setJbpmProcessInstanceId(long jbpmProcessInstanceId);
 
+	/**
+	 * Virtual fetch-group used by implementations that should include the current state. 
+	 */
 	static final String FETCH_GROUP_STATE = "StatableLocal.state";
+	/**
+	 * Virtual fetch-group used by implementations that should include the list of states. 
+	 */
 	static final String FETCH_GROUP_STATES = "StatableLocal.states";
 }

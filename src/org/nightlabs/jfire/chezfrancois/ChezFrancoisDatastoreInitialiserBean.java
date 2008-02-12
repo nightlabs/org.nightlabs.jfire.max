6e4a77dd135b6cdff156300148630f1956a9719f
tobias <tobias@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-02-12 14:31:58 +0000

    Cleanup for some projects.
    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireChezFrancois@9883 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/chezfrancois/ChezFrancoisDatastoreInitialiserBean.java
+++ src/org/nightlabs/jfire/chezfrancois/ChezFrancoisDatastoreInitialiserBean.java
@@ -62,6 +62,10 @@ extends BaseSessionBeanImpl
 implements SessionBean
 {
 	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	/**
 	 * LOG4J logger used by this class
 	 */
 	private static final Logger logger = Logger.getLogger(ChezFrancoisDatastoreInitialiserBean.class);

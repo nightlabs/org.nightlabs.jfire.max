7b2b94b3081836f587fb2be7e47a40783ee295e4
marius <marius@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-03-05 18:53:36 +0000

    some fixes and other crap
    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireIssueTracking@10039 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/issue/query/IssueQuery.java
+++ src/org/nightlabs/jfire/issue/query/IssueQuery.java
@@ -9,16 +9,20 @@ import org.apache.log4j.Logger;
 import org.nightlabs.jdo.ObjectID;
 import org.nightlabs.jdo.query.AbstractJDOQuery;
 import org.nightlabs.jfire.issue.Issue;
-import org.nightlabs.jfire.issue.IssueComment;
 import org.nightlabs.jfire.issue.id.IssuePriorityID;
 import org.nightlabs.jfire.issue.id.IssueResolutionID;
 import org.nightlabs.jfire.issue.id.IssueSeverityTypeID;
 import org.nightlabs.jfire.issue.id.IssueTypeID;
 import org.nightlabs.jfire.security.id.UserID;
 
+/**
+ * 
+ * @author Chairat Kongarayawetchakun <!-- chairat at nightlabs dot de -->
+ * @author Marius Heinzmann - marius[at]nightlabs[dot]com
+ */
 public class IssueQuery 
-extends AbstractJDOQuery<Issue> {
-	
+extends AbstractJDOQuery<Issue>
+{
 	private static final long serialVersionUID = 1L;
 
 	private static final Logger logger = Logger.getLogger(IssueQuery.class);
@@ -38,113 +42,123 @@ extends AbstractJDOQuery<Issue> {
 	
 	@Override
 	protected Query prepareQuery() {
-		Query q = getPersistenceManager().newQuery(Issue.class);
-		StringBuffer filter = new StringBuffer();
-		StringBuffer stringNames = new StringBuffer();
+		Query q = getPersistenceManager().newQuery(getResultType());
+		StringBuilder filter = new StringBuilder("true");
+//		StringBuffer stringNames = new StringBuffer();
 		
 		if (issueSubject != null) {
-			issueSubject = ".*" + issueSubject.toLowerCase() + ".*";
-			filter.append("( this.subject.names.containsValue(varSubject) && varSubject.toLowerCase().matches(\"" + issueSubject + "\") ) &&");
-			q.declareVariables(String.class.getName() + " varSubject");
+			filter.append("\n && subject.names.containsValue(varSubject) && varSubject.toLowerCase().matches(:issueSubject) ");
+//			q.declareVariables(String.class.getName() + " varSubject");
 		}
 		
 		if (issueComment != null) {
-			issueComment = ".*" + issueComment.toLowerCase() + ".*";
-			filter.append("( this.comments.contains(varComment) && varComment.text.toLowerCase().matches(\"" + issueComment + "\") ) &&");
-			q.declareVariables(IssueComment.class.getName() + " varComment");
+			filter.append("\n && comments.contains(varComment) && varComment.text.toLowerCase().matches(:issueComment) ");
+//			q.declareVariables(IssueComment.class.getName() + " varComment");
 		}
 		
 		if (issueSubjectNComment != null) {
-			issueSubjectNComment = ".*" + issueSubjectNComment.toLowerCase() + ".*";
-			filter.append("( this.comments.contains(varComment) && varComment.text.toLowerCase().matches(\"" + issueSubjectNComment + "\") ) &&");
-			filter.append("( this.subject.names.containsValue(varSubject) && varSubject.toLowerCase().matches(\"" + issueSubject + "\") ) &&");
-			q.declareVariables(IssueComment.class.getName() + " varComment");
-			q.declareVariables(String.class.getName() + " varSubject");
+			filter.append("\n && comments.contains(varComment) && varComment.text.toLowerCase().matches(:issueSubjectNComment) ");
+			filter.append("\n && subject.names.containsValue(varSubject) && varSubject.toLowerCase().matches(:issueSubject) ");
+//			q.declareVariables(IssueComment.class.getName() + " varComment");
+//			q.declareVariables(String.class.getName() + " varSubject");
 		}
 
 		if (issueTypeID != null) {
 //			filter.append("JDOHelper.getObjectId(this.issueType) == :issueTypeID && ");
-			filter.append("( this.issueType.organisationID == \"" + issueTypeID.organisationID + "\" && ");
-			filter.append("  this.issueType.issueTypeID == \"" + issueTypeID.issueTypeID + "\" ) && ");
+			filter.append("\n && issueType.organisationID == :issueTypeID.organisationID ");
+			filter.append("\n && issueType.issueTypeID == :issueTypeID.issueTypeID ");
 		}
 		
 		if (issueSeverityTypeID != null) {
-			filter.append("( this.issueSeverityType.organisationID == \"" + issueSeverityTypeID.organisationID + "\" && ");
-			filter.append("  this.issueSeverityType.issueSeverityTypeID == \"" + issueSeverityTypeID.issueSeverityTypeID + "\" ) && ");
+			filter.append("\n && this.issueSeverityType.organisationID == :issueSeverityTypeID.organisationID ");
+			filter.append("\n && this.issueSeverityType.issueSeverityTypeID == :issueSeverityTypeID.issueSeverityTypeID ");
 		}
 		
 		if (issuePriorityID != null) {
 			// FIXME: JPOX Bug JDOHelper.getObjectId(this.*) does not seem to work (java.lang.IndexOutOfBoundsException: Index: 3, Size: 3)
 //			filter.append("JDOHelper.getObjectId(this.issuePriority) == :issuePriorityID && ");
 			// WORKAROUND:
-			filter.append("( this.issuePriority.organisationID == \"" + issuePriorityID.organisationID + "\" && ");
-			filter.append("  this.issuePriority.issuePriorityID == \"" + issuePriorityID.issuePriorityID + "\" ) && ");
+			filter.append("\n && this.issuePriority.organisationID == :issuePriorityID.organisationID ");
+			filter.append("\n &&  this.issuePriority.issuePriorityID == :issuePriorityID.issuePriorityID ");
 		}
 		
 		if (issueResolutionID != null) {
 			// FIXME: JPOX Bug JDOHelper.getObjectId(this.*) does not seem to work (java.lang.IndexOutOfBoundsException: Index: 3, Size: 3)
 //			filter.append("JDOHelper.getObjectId(this.issueResolution) == :issueResolutionID && ");
 			// WORKAROUND:
-			filter.append("( this.issueResolution.organisationID == \"" + issueResolutionID.organisationID + "\" && ");
-			filter.append("  this.issueResolution.issueResolutionID == \"" + issueResolutionID.issueResolutionID + "\" ) && ");
+			filter.append("\n && this.issueResolution.organisationID == :issueResolutionID.organisationID ");
+			filter.append("\n && this.issueResolution.issueResolutionID == :issueResolutionID.issueResolutionID ");
 		}
 		
 		if (reporterID != null) {
 //			filter.append("JDOHelper.getObjectId(this.reporter) == :reporterID && ");
-			filter.append("( this.reporter.organisationID == \"" + reporterID.organisationID + "\" && ");
-			filter.append("  this.reporter.userID == \"" + reporterID.userID + "\" ) && ");
+			filter.append("\n && this.reporter.organisationID == :reporterID.organisationID ");
+			filter.append("\n && this.reporter.userID == :reporterID.userID ");
 		}
 		
 		if (assigneeID != null) {
 //			filter.append("JDOHelper.getObjectId(this.assignee) == :assigneeID && ");
-			filter.append("( this.assignee.organisationID == \"" + assigneeID.organisationID + "\" && ");
-			filter.append("  this.assignee.userID == \"" + assigneeID.userID + "\" ) && ");
+			filter.append("\n && this.assignee.organisationID == :assigneeID.organisationID ");
+			filter.append("\n && this.assignee.userID == :assigneeID.userID ");
 		}
 	
 		if (createTimestamp != null) {
 //			filter.append("JDOHelper.getObjectId(this.createTimestamp) == :createTimestamp && ");
-			filter.append("( this.createTimestamp >= :createTimestamp ) &&");
+			filter.append("\n && this.createTimestamp >= :createTimestamp ");
 		}
 		
 		if (updateTimestamp != null) {
 //			filter.append("JDOHelper.getObjectId(this.updateTimestamp) == :updateTimestamp && ");
-			filter.append("( this.updateTimestamp >= :updateTimestamp ) &&");
+			filter.append("\n && this.updateTimestamp >= :updateTimestamp ");
 		}
 		
-		if (objectIDs != null) {
-			for (int i = 0; i < objectIDs.size(); i++) {
-				ObjectID objectID = objectIDs.iterator().next();
-				String objectIDString = objectID.toString();
-				filter.append("( this.referencedObjectIDs.contains(varObjectID"+i+") && varObjectID"+i+".matches(\"" + objectIDString + "\") )");
-				stringNames.append(String.class.getName() + " varObjectID" + i);
-				
-				if (i != objectIDs.size() - 1) {
-					stringNames.append(";");
-					filter.append("||");
-				}
-			}
-			
-			q.declareVariables(stringNames.toString());
-			
-			filter.append("&& ");
-		}
+		// FIXME: chairat please rewrite this part as soon as you have refactored the linkage of objects to Issues. (marius)
+//		if (objectIDs != null && !objectIDs.isEmpty())
+//		{
+//			filter.append("\n && ( ");
+//			filter.append("\n \t this.referencedObjectIDs.contains(varObjectID) && \n \t (");
+//			for (ObjectID objectID : objectIDs)
+//			{
+//				String objectIDString = objectID.toString();
+//				filter.append("\n \t \t varObjectID.matches(" + objectIDString + ") ||");
+//			}
+//			filter.delete(filter.length() - 2, filter.length());
+//			filter.append("\n \t )");
+//			filter.append("\n && )");
+//		}
 		
-		filter.append("(1 == 1)");
 		logger.info(filter.toString());
 		q.setFilter(filter.toString());
 		return q;
 	}
 
-	public String getIssueSubject() {
+	public String getIssueSubject()
+	{
 		return issueSubject;
 	}
 	
-	public void setIssueSubject(String issueSubject) {
-		this.issueSubject = issueSubject;
+	public void setIssueSubject(String issueSubject)
+	{
+		if (issueSubject == null || issueSubject.length() == 0)
+		{
+			this.issueSubject = null;
+		}
+		else
+		{
+			this.issueSubject = ".*" + issueSubject.toLowerCase() + ".*";
+		}
 	}
 	
-	public void setIssueSubjectNComment(String issueSubjectNComment) {
-		this.issueSubjectNComment = issueSubjectNComment;
+	public void setIssueSubjectNComment(String issueSubjectNComment)
+	{
+		if (issueSubjectNComment == null || issueSubjectNComment.length() == 0)
+		{
+			this.issueSubjectNComment = null;			
+		}
+		else
+		{
+			this.issueSubjectNComment = ".*" + issueSubjectNComment.toLowerCase() + ".*";
+		}
 	}
 	
 	public String getIssueSubjectNComment() {
@@ -155,8 +169,16 @@ extends AbstractJDOQuery<Issue> {
 		return issueComment;
 	}
 	
-	public void setIssueComment(String issueComment) {
-		this.issueComment = issueComment;
+	public void setIssueComment(String issueComment)
+	{
+		if (issueComment == null || issueComment.length() == 0)
+		{
+			this.issueComment = null;
+		}
+		else
+		{
+			this.issueComment = ".*" + issueComment.toLowerCase() + ".*";
+		}
 	}
 	
 	public IssueTypeID getIssueTypeID() {

7f598b5ee5a0dda95bcd51fc292839c3e9612432
nozkiller <nozkiller@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-03-14 17:38:08 +0000

    converted some old JDOObjectProvider to BaseJDOObjectDAO and deleted them + generics cleanup
    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireTrade@10113 18c61ebf-f106-0410-9e1e-81fb82415b8f

---
+++ src/org/nightlabs/jfire/accounting/dao/LocalAccountantDelegateDAO.java
@@ -0,0 +1,107 @@
+/**
+ * 
+ */
+package org.nightlabs.jfire.accounting.dao;
+
+import java.util.Collection;
+import java.util.Set;
+
+import org.nightlabs.jfire.accounting.AccountingManager;
+import org.nightlabs.jfire.accounting.AccountingManagerUtil;
+import org.nightlabs.jfire.accounting.book.LocalAccountantDelegate;
+import org.nightlabs.jfire.accounting.book.id.LocalAccountantDelegateID;
+import org.nightlabs.jfire.base.jdo.BaseJDOObjectDAO;
+import org.nightlabs.jfire.security.SecurityReflector;
+import org.nightlabs.progress.ProgressMonitor;
+
+/**
+ * @author Daniel Mazurek - daniel [at] nightlabs [dot] de
+ *
+ */
+public class LocalAccountantDelegateDAO 
+extends BaseJDOObjectDAO<LocalAccountantDelegateID, LocalAccountantDelegate>
+{
+	private static LocalAccountantDelegateDAO sharedInstance = null;
+
+	public static LocalAccountantDelegateDAO sharedInstance()
+	{
+		if (sharedInstance == null) {
+			synchronized (LocalAccountantDelegateDAO.class) {
+				if (sharedInstance == null)
+					sharedInstance = new LocalAccountantDelegateDAO();
+			}
+		}
+		return sharedInstance;
+	}
+	
+	private LocalAccountantDelegateDAO() {
+	}
+
+	/* (non-Javadoc)
+	 * @see org.nightlabs.jfire.base.jdo.BaseJDOObjectDAO#retrieveJDOObjects(java.util.Set, java.lang.String[], int, org.nightlabs.progress.ProgressMonitor)
+	 */
+	@Override
+	protected Collection<LocalAccountantDelegate> retrieveJDOObjects(Set<LocalAccountantDelegateID> delegateIDs, 
+			String[] fetchGroups, int maxFetchDepth, ProgressMonitor monitor) throws Exception 
+	{
+		monitor.beginTask("Loading LocalAccountDelegates", 1);
+		try {
+			AccountingManager am = AccountingManagerUtil.getHome(SecurityReflector.getInitialContextProperties()).create();  
+			return am.getLocalAccountantDelegates(delegateIDs, fetchGroups, maxFetchDepth);
+		} catch (Exception e) {
+			monitor.setCanceled(true);
+			throw e;
+		} finally {
+			monitor.worked(1);
+			monitor.done();
+		}
+	}
+
+	/**
+	 * Get the LocalAccountantDelegate for the given delegateID.
+	 * 
+	 * @param delegateID The delegateID of the desired LocalAccountantDelegate.
+	 * @param fetchGroups The fetchGroups to detach the delegate with.
+	 * @return A cached version of the LocalAccountantDelegate with the given id.
+	 */
+	public LocalAccountantDelegate getDelegate(LocalAccountantDelegateID delegateID, 
+			String[] fetchGroups, int maxFetchDepth, ProgressMonitor monitor) 
+	{
+		return getJDOObject(null, delegateID, fetchGroups, maxFetchDepth, monitor);
+	}
+	
+	public Collection<LocalAccountantDelegate> getTopLevelDelegates(Class delegateClass, 
+			String[] fetchGroups, int maxFetchDepth, ProgressMonitor monitor) 
+	{
+		try {
+			AccountingManager am = AccountingManagerUtil.getHome(SecurityReflector.getInitialContextProperties()).create();
+			return getJDOObjects(
+					null, am.getTopLevelAccountantDelegates(delegateClass),
+					fetchGroups, maxFetchDepth, monitor
+				);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	/**
+	 * Get all LocalAccountantDelegates that have the delegate with the given
+	 * id as extendedLocalAccountantDelegate.
+	 * 
+	 * @param delegateID The parent delegate id.
+	 * @param fetchGroups The fetchGroups to detach the children with.
+	 */
+	public Collection<LocalAccountantDelegate> getChildDelegates(LocalAccountantDelegateID delegateID, 
+			String[] fetchGroups, int maxFetchDepth, ProgressMonitor monitor) 
+	{
+		try {
+			AccountingManager am = AccountingManagerUtil.getHome(SecurityReflector.getInitialContextProperties()).create();
+			return getJDOObjects(
+					null, am.getChildAccountantDelegates(delegateID),
+					fetchGroups, maxFetchDepth, monitor
+				);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}	
+}

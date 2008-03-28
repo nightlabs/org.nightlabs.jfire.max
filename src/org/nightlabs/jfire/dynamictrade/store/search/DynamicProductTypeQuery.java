2982bc55f23fe9716812c012c83ca6522aeafd80
marius <marius@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-03-28 17:31:22 +0000

    - removed some warnings - removed unnecessary FormPage for the real page body inside the EntityEditorFormPageWithProgress - advanced in the QueryStore-API
    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireDynamicTrade@10212 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/dynamictrade/store/search/DynamicProductTypeQuery.java
+++ src/org/nightlabs/jfire/dynamictrade/store/search/DynamicProductTypeQuery.java
@@ -1,7 +1,5 @@
 package org.nightlabs.jfire.dynamictrade.store.search;
 
-import javax.jdo.Query;
-
 import org.nightlabs.jfire.dynamictrade.store.DynamicProductType;
 import org.nightlabs.jfire.store.search.AbstractProductTypeQuery;
 
@@ -14,19 +12,19 @@ extends AbstractProductTypeQuery<DynamicProductType>
 {
 	private static final long serialVersionUID = 1L;
 
-	@Override
-	protected Query prepareQuery()
-	{
-		super.prepareQuery();
-		// FIXME: Query also subclasses when JPOX problem is solved
-		Query q = getPersistenceManager().newQuery(getPersistenceManager().getExtent(
-				DynamicProductType.class, false));
-		
-		q.setFilter(getFilter().toString());
-		q.declareVariables(getVars().toString());
-
-		return q;
-	}
+//	@Override
+//	protected Query prepareQuery()
+//	{
+//		Query q = super.prepareQuery();
+//		// FIXME: Query also subclasses when JPOX problem is solved
+////		 = getPersistenceManager().newQuery(getPersistenceManager().getExtent(
+////				DynamicProductType.class, false));
+//		
+//		q.setFilter(getFilter().toString());
+//		q.declareVariables(getVars().toString());
+//
+//		return q;
+//	}
 
 	@Override
 	protected Class<DynamicProductType> init()

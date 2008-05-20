0ebe5b35e2dce14e6addf33a3da1b407caf61a84
nozkiller <nozkiller@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-05-20 11:56:07 +0000

    Initial import.
    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireVoucherEditor2D@10575 18c61ebf-f106-0410-9e1e-81fb82415b8f

---
+++ src/org/nightlabs/jfire/voucher/editor2d/iofilter/VoucherXStreamFilter.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package org.nightlabs.jfire.voucher.editor2d.iofilter;
+
+import org.nightlabs.editor2d.iofilter.ManifestWriter;
+import org.nightlabs.editor2d.iofilter.XStreamFilter;
+
+/**
+ * @author Daniel Mazurek - daniel [at] nightlabs [dot] de
+ *
+ */
+public class VoucherXStreamFilter 
+extends XStreamFilter 
+{
+	public static final String FILE_EXTENSION = "v2d"; //$NON-NLS-1$	
+	public static final String VOUCHER_CONTENT_TYPE = "vnd.nightlabs.jfire.voucher"; //$NON-NLS-1$
+	
+	@Override
+	protected String initDescription() {
+		return "JFire Voucher Design File Format";
+	}
+
+	@Override
+	protected String[] initFileExtensions() {
+		return new String[] { FILE_EXTENSION };
+	}
+
+	@Override
+	protected String initName() {
+		return "JFire Voucher Design";
+	}
+
+	@Override
+	protected ManifestWriter createManifestWriter() {
+		ManifestWriter manifestWriter = super.createManifestWriter();
+		manifestWriter.setContentType(VOUCHER_CONTENT_TYPE);
+		return manifestWriter;
+	}
+	
+}

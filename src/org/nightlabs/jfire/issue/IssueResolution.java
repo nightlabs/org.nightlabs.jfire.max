370d69c96af7a8138d3ab6c8ece1dc0f8e583d47
marco <marco@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-06-16 17:18:49 +0000

    1.  The constant ISSUE_RESOLUTION_ID_NOT_ASSIGNED was defined with the String "Not Assigned". I removed the space, since it was illegal.
    2. I removed the @jdo.fetch-group for "IssueResolution.this".
    
    3. equals and hashCode methods were wrong! They must always include all primary key fields. I added the organisationID to them.
    
    4. added a toString method.
    
    
    
    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireIssueTracking@10912 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/issue/IssueResolution.java
+++ src/org/nightlabs/jfire/issue/IssueResolution.java
@@ -2,13 +2,15 @@ package org.nightlabs.jfire.issue;
 
 import java.io.Serializable;
 
-import org.nightlabs.jfire.issue.id.IssueLinkTypeID;
+import org.nightlabs.jdo.ObjectIDUtil;
 import org.nightlabs.jfire.issue.id.IssueResolutionID;
 import org.nightlabs.jfire.organisation.Organisation;
 import org.nightlabs.util.Util;
 
 /**
  * @author Alexander Bieber <!-- alex [AT] nightlabs [DOT] de -->
+ * @author Chairat Kongarayawetchakun - chairat at nightlabs dot de
+ * @author Marco Schulze - marco at nightlabs dot de
  *
  * @jdo.persistence-capable
  *		identity-type = "application"
@@ -21,15 +23,14 @@ import org.nightlabs.util.Util;
  * @jdo.inheritance strategy = "new-table"
  * 
  * @jdo.fetch-group name="IssueResolution.name" fetch-groups="default" fields="name"
- * @jdo.fetch-group name="IssueResolution.this" fetch-groups="default" fields="name"
  */
 public class IssueResolution
 implements Serializable
 {
 	public static final String FETCH_GROUP_NAME = "IssueResolution.name";
-	
-	public static final IssueResolutionID ISSUE_RESOLUTION_ID_NOT_ASSIGNED = IssueResolutionID.create(Organisation.DEV_ORGANISATION_ID, "Not assigned");
-	
+
+	public static final IssueResolutionID ISSUE_RESOLUTION_ID_NOT_ASSIGNED = IssueResolutionID.create(Organisation.DEV_ORGANISATION_ID, "NotAssigned");
+
 	private static final long serialVersionUID = 1L;
 	/**
 	 * @jdo.field primary-key="true"
@@ -55,17 +56,15 @@ implements Serializable
 	{
 	}
 
-	public IssueResolution(String organisationID, String issueResolutionID){
-		if (organisationID == null)
-			throw new IllegalArgumentException("organisationID must not be null!");
-		if (issueResolutionID == null)
-			throw new IllegalArgumentException("issueResolutinoID must not be null!");
+	public IssueResolution(String organisationID, String issueResolutionID) {
+		Organisation.assertValidOrganisationID(organisationID);
+		ObjectIDUtil.assertValidIDString(issueResolutionID, "issueResolutionID");
 
 		this.organisationID = organisationID;
 		this.issueResolutionID = issueResolutionID;
 		this.name = new IssueResolutionName(this);
 	}
-	
+
 	public String getOrganisationID() {
 		return organisationID;
 	}
@@ -87,14 +86,23 @@ implements Serializable
 	public boolean equals(Object obj)
 	{
 		if (obj == this) return true;
-		if (!(obj instanceof IssueResolution)) return false;
+		if (obj == null) return false;
+		if (obj.getClass() != this.getClass()) return false;
 		IssueResolution o = (IssueResolution) obj;
-		return Util.equals(o.issueResolutionID, this.issueResolutionID);
+		return (
+				Util.equals(o.organisationID, this.organisationID) &&
+				Util.equals(o.issueResolutionID, this.issueResolutionID)
+		);
 	}
 
 	@Override
 	public int hashCode()
 	{
-		return Util.hashCode(issueResolutionID);
+		return (31 * Util.hashCode(organisationID)) + Util.hashCode(issueResolutionID);
+	}
+
+	@Override
+	public String toString() {
+		return this.getClass().getName() + '@' + Integer.toHexString(System.identityHashCode(this)) + '[' + organisationID + ',' + issueResolutionID + ']';
 	}
 }

3e6faf4e5494d6ebdcbdf6a16ed4ab2ad5871b68
marco <marco@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-07-04 15:18:05 +0000

    * switched version number from 0.9.3 to 0.9.4 (some plugins other versions, but all incremented to an even number) * added ".qualifier" to all RCP plugins where it was still missing * updated build-nightlabs.xml in most RCP plugins * removed "publishes" in nightly.xml files
    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireSimpleTrade@11267 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/simpletrade/SimpleTradeManagerBean.java
+++ src/org/nightlabs/jfire/simpletrade/SimpleTradeManagerBean.java
@@ -195,7 +195,7 @@ implements SessionBean
 
 			// version is {major}.{minor}.{release}-{patchlevel}-{suffix}
 			moduleMetaData = new ModuleMetaData(
-					"JFireSimpleTrade", "0.9.3-0-beta", "0.9.3-0-beta");
+					"JFireSimpleTrade", "0.9.4-0-beta", "0.9.4-0-beta");
 			pm.makePersistent(moduleMetaData);
 			
 			SimpleProductTypeStruct.getSimpleProductTypeStruct(organisationID, pm);

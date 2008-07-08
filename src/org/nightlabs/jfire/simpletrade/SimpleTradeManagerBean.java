826fc5ca1ec25a6362c18e500a6c871d7b73b651
marco <marco@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-07-08 20:29:47 +0000

    switched version number from 0.9.4 to 0.9.5 (or otherwise increased, if the version number is different - like in the editor for example)
    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireSimpleTrade@11340 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/simpletrade/SimpleTradeManagerBean.java
+++ src/org/nightlabs/jfire/simpletrade/SimpleTradeManagerBean.java
@@ -195,7 +195,7 @@ implements SessionBean
 
 			// version is {major}.{minor}.{release}-{patchlevel}-{suffix}
 			moduleMetaData = new ModuleMetaData(
-					JFireSimpleTradeEAR.MODULE_NAME, "0.9.4-0-beta", "0.9.4-0-beta");
+					JFireSimpleTradeEAR.MODULE_NAME, "0.9.5-0-beta", "0.9.5-0-beta");
 			pm.makePersistent(moduleMetaData);
 			
 			SimpleProductTypeStruct.getSimpleProductTypeStruct(organisationID, pm);

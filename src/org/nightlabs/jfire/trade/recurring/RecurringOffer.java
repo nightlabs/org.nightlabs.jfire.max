ec756d6012651d0824e315d1ac6f4a73accc1574
fitas_java <fitas_java@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-07-21 09:03:52 +0000

    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireTrade@11458 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/trade/recurring/RecurringOffer.java
+++ src/org/nightlabs/jfire/trade/recurring/RecurringOffer.java
@@ -1,8 +1,15 @@
 package org.nightlabs.jfire.trade.recurring;
 
+import org.nightlabs.jfire.organisation.Organisation;
 import org.nightlabs.jfire.security.User;
+import org.nightlabs.jfire.store.Product;
+import org.nightlabs.jfire.store.ProductTypeActionHandler;
 import org.nightlabs.jfire.trade.Offer;
 import org.nightlabs.jfire.trade.Order;
+
+
+
+
 
 public class RecurringOffer extends Offer {
 
--- src/org/nightlabs/jfire/trade/recurring/RecurringOrder.java
+++ src/org/nightlabs/jfire/trade/recurring/RecurringOrder.java
@@ -1,6 +1,23 @@
 package org.nightlabs.jfire.trade.recurring;
 
 import org.nightlabs.jfire.trade.Order;
+/**
+ *
+ * @author Fitas Amine <fitas@nightlabs.de>
+
+ * @jdo.persistence-capable
+ *		identity-type="application"
+ *		objectid-class="org.nightlabs.jfire.id.RecurringOrderID"
+ *		detachable="true"
+ *		table="JFireTrade_Recurring"
+ *
+ * @jdo.version strategy="version-number"
+ *
+ * @jdo.inheritance strategy="new-table"
+ * @jdo.inheritance-discriminator strategy="class-name"
+ *
+ *
+ **/
 
 public class RecurringOrder extends Order {
 

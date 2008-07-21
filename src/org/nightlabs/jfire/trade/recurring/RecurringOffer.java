af9c88852ba6457ac858a6af4236d6ab5ac864be
fitas_java <fitas_java@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-07-21 08:50:27 +0000

    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireTrade@11457 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/trade/recurring/RecurringOffer.java
+++ src/org/nightlabs/jfire/trade/recurring/RecurringOffer.java
@@ -1,9 +1,22 @@
 package org.nightlabs.jfire.trade.recurring;
 
+import org.nightlabs.jfire.security.User;
 import org.nightlabs.jfire.trade.Offer;
+import org.nightlabs.jfire.trade.Order;
 
 public class RecurringOffer extends Offer {
 
+	private RecurringOfferConfiguration recurringOfferConfiguration;
+	
+	
+	public RecurringOffer(User user, Order order, String offerIDPrefix,
+			long offerID) {
+		super(user, order, offerIDPrefix, offerID);
+		
+		recurringOfferConfiguration = null;
+		
+	}
 
 
+	
 }
--- src/org/nightlabs/jfire/trade/recurring/RecurringOfferConfiguration.java
+++ src/org/nightlabs/jfire/trade/recurring/RecurringOfferConfiguration.java
@@ -2,4 +2,16 @@ package org.nightlabs.jfire.trade.recurring;
 
 public class RecurringOfferConfiguration {
 
+
+
+	boolean createInvoice()
+	{
+		return false;
+	}
+
+	boolean createDelivery()
+	{
+		return false;
+	}
+	
 }

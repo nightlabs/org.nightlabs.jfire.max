67b1a435ec92aae035aea432b4f63fef0a58f12d
fitas_java <fitas_java@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-07-23 13:06:53 +0000

    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireTrade@11497 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/trade/recurring/RecurringTrader.java
+++ src/org/nightlabs/jfire/trade/recurring/RecurringTrader.java
@@ -1,8 +1,21 @@
 package org.nightlabs.jfire.trade.recurring;
 
 import java.util.Iterator;
+
+import javax.jdo.JDOHelper;
 import javax.jdo.PersistenceManager;
+
+import org.nightlabs.ModuleException;
+import org.nightlabs.jfire.accounting.Currency;
+import org.nightlabs.jfire.config.Config;
+import org.nightlabs.jfire.idgenerator.IDGenerator;
 import org.nightlabs.jfire.security.SecurityReflector;
+import org.nightlabs.jfire.security.User;
+import org.nightlabs.jfire.trade.LegalEntity;
+import org.nightlabs.jfire.trade.Order;
+import org.nightlabs.jfire.trade.OrganisationLegalEntity;
+import org.nightlabs.jfire.trade.Trader;
+import org.nightlabs.jfire.trade.config.TradeConfigModule;
 
 
 /**
@@ -61,6 +74,64 @@ public class RecurringTrader {
 	public String getOrganisationID() {
 		return organisationID;
 	}
+	
+	protected PersistenceManager getPersistenceManager()
+	{
+		PersistenceManager pm = JDOHelper.getPersistenceManager(this);
+		if (pm == null)
+			throw new IllegalStateException(
+					"This instance of Trader is currently not attached to a datastore! Cannot get a PersistenceManager!");
 
+		return pm;
+	}
+	
+	public String getOrderIDPrefix(User user, String orderIDPrefix)
+	{
+		if (orderIDPrefix == null) {
+			TradeConfigModule tradeConfigModule;
+			try {
+				tradeConfigModule = (TradeConfigModule) Config.getConfig(
+						getPersistenceManager(), getOrganisationID(), user).createConfigModule(TradeConfigModule.class);
+			} catch (ModuleException x) {
+				throw new RuntimeException(x); // should not happen.
+			}
 
+			orderIDPrefix = tradeConfigModule.getActiveIDPrefixCf(RecurringOrder.class.getName()).getDefaultIDPrefix();
+		}
+		return orderIDPrefix;
+	}
+	
+	
+	public RecurringOrder createRecurringOrder(LegalEntity vendor,
+			LegalEntity customer, String orderIDPrefix, Currency currency)
+//	throws ModuleException
+	{
+		if (customer == null)
+			throw new IllegalArgumentException("customer must not be null!");
+
+		if (currency == null)
+			throw new IllegalArgumentException("currency must not be null!");
+
+		PersistenceManager pm = getPersistenceManager();
+		Trader trader = Trader.getTrader(pm);
+		
+		if (!trader.getMandator().equals(vendor) && (vendor instanceof OrganisationLegalEntity)) {
+			// TODO: Implement foreign stuff
+			throw new UnsupportedOperationException("NYI");
+		}
+		else {
+			// local: the vendor is the local organisation (owning this datastore) OR it is a locally managed non-organisation-LE
+			User user = SecurityReflector.getUserDescriptor().getUser(pm);
+
+			orderIDPrefix = getOrderIDPrefix(user, orderIDPrefix);
+
+			RecurringOrder recurringOrder = new RecurringOrder(
+					vendor, customer,
+					orderIDPrefix, IDGenerator.nextID(Order.class, orderIDPrefix),
+					currency, user);
+
+			recurringOrder = getPersistenceManager().makePersistent(recurringOrder);
+			return recurringOrder;
+		}
+	}
 }

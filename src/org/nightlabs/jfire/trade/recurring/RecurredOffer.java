df1ac1563239c41486f8af5fd71a2b32d969765a
fitas_java <fitas_java@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-07-23 10:29:23 +0000

    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireTrade@11495 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/trade/recurring/RecurredOffer.java
+++ src/org/nightlabs/jfire/trade/recurring/RecurredOffer.java
@@ -1,6 +1,22 @@
 package org.nightlabs.jfire.trade.recurring;
 
-public class RecurredOffer {
+import org.nightlabs.jfire.trade.Offer;
+/**
+ * @author Fitas Amine <fitas@nightlabs.de>
+ * @jdo.persistence-capable
+ *		identity-type="application"
+ *		persistence-capable-superclass="org.nightlabs.jfire.trade.Offer"
+ *		detachable="true"
+ *		table="JFireTrade_RecurredOffer"
+ *
+ * @jdo.inheritance strategy="new-table"
+ * 
+ * @jdo.fetch-group name="RecurringOffer.recurringOffer" fields="recurringOffer"
+ * 
+ * 
+ * 
+ */
+public class RecurredOffer extends Offer{
 
 	private RecurringOffer recurringOffer;
 

94a0f47813051083fa80b03d7aaea3721db52f33
fitas_java <fitas_java@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-07-23 10:06:29 +0000

    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireTrade@11494 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/trade/recurring/RecurringTrader.java
+++ src/org/nightlabs/jfire/trade/recurring/RecurringTrader.java
@@ -1,17 +1,9 @@
 package org.nightlabs.jfire.trade.recurring;
 
 import java.util.Iterator;
-import java.util.Locale;
-
 import javax.jdo.PersistenceManager;
-
-import org.apache.log4j.Logger;
-import org.nightlabs.jfire.accounting.Accounting;
 import org.nightlabs.jfire.security.SecurityReflector;
-import org.nightlabs.jfire.store.Store;
-import org.nightlabs.jfire.trade.CustomerGroup;
-import org.nightlabs.jfire.trade.LegalEntity;
-import org.nightlabs.jfire.trade.OrganisationLegalEntity;
+
 
 /**
  * RecurringTrader is responsible for purchase and sale of recurring orders and offers
@@ -31,31 +23,10 @@ import org.nightlabs.jfire.trade.OrganisationLegalEntity;
 public class RecurringTrader {
 
 	/**
-	 * LOG4J logger used by this class
-	 */
-	private static final Logger logger = Logger.getLogger(RecurringTrader.class);
-
-	/**
 	 * @jdo.field primary-key="true"
 	 * @jdo.column length="100"
 	 */
 	private String organisationID;
-
-	/**
-	 * @jdo.field persistence-modifier="persistent"
-	 */
-	private Accounting accounting;
-
-	/**
-	 * @jdo.field persistence-modifier="persistent"
-	 */
-	private Store store;
-
-	/**
-	 * The mandator is the LegalEntity that is represented by this Trader.
-	 */
-	private OrganisationLegalEntity mandator;
-
 
 
 	/**
@@ -69,90 +40,26 @@ public class RecurringTrader {
 	{
 		Iterator<?> it = pm.getExtent(RecurringTrader.class).iterator();
 		if (it.hasNext()) {
-			RecurringTrader trader = (RecurringTrader) it.next();
+			RecurringTrader recurringTrader = (RecurringTrader) it.next();
 			// TODO remove this debug stuff
 			String securityReflectorOrganisationID = SecurityReflector.getUserDescriptor().getOrganisationID();
-			if (!securityReflectorOrganisationID.equals(trader.getOrganisationID()))
-				throw new IllegalStateException("SecurityReflector returned organisationID " + securityReflectorOrganisationID + " but Trader.organisationID=" + trader.getOrganisationID());
+			if (!securityReflectorOrganisationID.equals(recurringTrader.getOrganisationID()))
+				throw new IllegalStateException("SecurityReflector returned organisationID " + securityReflectorOrganisationID + " but Trader.organisationID=" + recurringTrader.getOrganisationID());
 			// TODO end debug
-			return trader;
+			return recurringTrader;
 		}
 
-		logger.info("getTrader: The Trader instance does not yet exist! Creating it...");
 
 		RecurringTrader recurringTrader = new RecurringTrader();
-
-		recurringTrader.store = Store.getStore(pm);
-		recurringTrader.accounting = Accounting.getAccounting(pm);
-		recurringTrader.organisationID = recurringTrader.accounting.getOrganisationID();
-		recurringTrader.mandator = recurringTrader.accounting.getMandator();
-
-		// create customer groups
-		CustomerGroup anonymousCustomerGroup = new CustomerGroup(recurringTrader.organisationID, CustomerGroup.CUSTOMER_GROUP_ID_ANONYMOUS);
-		anonymousCustomerGroup.getName().setText(Locale.GERMAN.getLanguage(), "Anonym");
-		anonymousCustomerGroup.getName().setText(Locale.FRENCH.getLanguage(), "Anonyme");
-		anonymousCustomerGroup.getName().setText(Locale.ENGLISH.getLanguage(), "Anonymous");
-		anonymousCustomerGroup = pm.makePersistent(anonymousCustomerGroup);
-
-		CustomerGroup resellerCustomerGroup = new CustomerGroup(recurringTrader.organisationID, CustomerGroup.CUSTOMER_GROUP_ID_RESELLER);
-		resellerCustomerGroup.getName().setText(Locale.GERMAN.getLanguage(), "Wiederverkäufer");
-		resellerCustomerGroup.getName().setText(Locale.ENGLISH.getLanguage(), "Reseller");
-		resellerCustomerGroup = pm.makePersistent(resellerCustomerGroup);
-
-		CustomerGroup defaultCustomerGroup = new CustomerGroup(recurringTrader.organisationID, CustomerGroup.CUSTOMER_GROUP_ID_DEFAULT);
-		defaultCustomerGroup.getName().setText(Locale.GERMAN.getLanguage(), "Standard");
-		defaultCustomerGroup.getName().setText(Locale.FRENCH.getLanguage(), "Standard");
-		defaultCustomerGroup.getName().setText(Locale.ENGLISH.getLanguage(), "Default");
-		defaultCustomerGroup = pm.makePersistent(defaultCustomerGroup);
-
-		recurringTrader.defaultCustomerGroupForKnownCustomer = defaultCustomerGroup;
+		recurringTrader.organisationID = SecurityReflector.getUserDescriptor().getOrganisationID();
 		recurringTrader = pm.makePersistent(recurringTrader);
 
-		// Normally this is done by OrganisationLegalEntity.getOrganisationLegalEntity(...), but since this would cause an endless recursion, we skip it in this situation
-		// there and do it here.
-		recurringTrader.mandator.setDefaultCustomerGroup(defaultCustomerGroup);
-
-		logger.info("getTrader: ...new Trader instance created and persisted!");
 		return recurringTrader;
 	}
 
 
-
 	public String getOrganisationID() {
 		return organisationID;
-	}
-
-
-	public Accounting getAccounting() {
-		return accounting;
-	}
-
-
-
-	public Store getStore() {
-		return store;
-	}
-
-
-
-	public OrganisationLegalEntity getMandator() {
-		return mandator;
-	}
-
-	/**
-	 * @jdo.field persistence-modifier="persistent"
-	 */
-	private CustomerGroup defaultCustomerGroupForKnownCustomer;
-
-	/**
-	 * @return Returns the <tt>CustomerGroup</tt> that is automatically assigned
-	 *         to all newly created customers as
-	 *         {@link LegalEntity#defaultCustomerGroup}. Note, that the anonymous
-	 *         customer has a different <tt>CustomerGroup</tt> assigned!
-	 */
-	public CustomerGroup getDefaultCustomerGroupForKnownCustomer()
-	{
-		return defaultCustomerGroupForKnownCustomer;
 	}
 
 

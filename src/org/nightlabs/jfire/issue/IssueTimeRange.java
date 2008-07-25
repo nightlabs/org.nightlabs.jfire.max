7c8d1d5d357159554bb5611e4659a4be5b872dd8
chairatk <chairatk@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-07-25 10:26:42 +0000

    added IssueTimeRange class.
    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireIssueTracking@11510 18c61ebf-f106-0410-9e1e-81fb82415b8f

---
+++ src/org/nightlabs/jfire/issue/IssueTimeRange.java
@@ -0,0 +1,154 @@
+package org.nightlabs.jfire.issue;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.jdo.JDOHelper;
+import javax.jdo.PersistenceManager;
+
+import org.apache.log4j.Logger;
+import org.nightlabs.jfire.security.User;
+import org.nightlabs.util.Util;
+
+/**
+ * @author Chairat Kongarayawetchakun <!-- chairat at nightlabs dot de -->
+ *
+ * @jdo.persistence-capable
+ *		identity-type="application"
+ *		objectid-class="org.nightlabs.jfire.issue.history.id.IssueTimeRangeID"
+ *		detachable="true"
+ *		table="JFireIssueTracking_IssueTimeRange"
+ *
+ * @jdo.inheritance strategy="new-table"
+ *
+ * @jdo.create-objectid-class
+ *		field-order="organisationID, issueID, issueTimeRangeID"
+ *
+ * @jdo.fetch-group name="IssueTimeRange.issue" fields="issue"
+ * @jdo.fetch-group name="IssueTimeRange.this" fetch-groups="default" fields="createTimestamp"
+ *
+ **/
+public class IssueTimeRange
+implements Serializable 
+{
+	private static final long serialVersionUID = 1L;
+	private static final Logger logger = Logger.getLogger(IssueTimeRange.class);
+
+	/**
+	 * @deprecated The *.this-FetchGroups lead to bad programming style and are therefore deprecated, now. They should be removed soon! 
+	 */
+	public static final String FETCH_GROUP_THIS = "IssueTimeRange.this";
+	public static final String FETCH_GROUP_ISSUE = "IssueTimeRange.issue";
+	public static final String FETCH_GROUP_USER = "IssueTimeRange.user";
+	
+	/**
+	 * @jdo.field primary-key="true"
+	 * @jdo.column length="100"
+	 */
+	private String organisationID;
+
+	/**
+	 * @jdo.field primary-key="true"
+	 */
+	private long issueID;
+
+	/**
+	 * @jdo.field primary-key="true"
+	 */
+	private long issueTimeRangeID;
+
+	/**
+	 * @jdo.field persistence-modifier="persistent"
+	 * @jdo.column length="100"
+	 */
+	private String field;
+
+	/**
+	 * @jdo.field persistence-modifier="persistent"
+	 * @jdo.column length="255"
+	 */
+	private String change;
+
+	/**
+	 * @jdo.field persistence-modifier="persistent"
+	 */
+	private Issue issue;
+
+	/**
+	 * @jdo.field persistence-modifier="persistent"
+	 */
+	private Date createTimestamp;
+
+	/**
+	 * @deprecated Constructor exists only for JDO! 
+	 */
+	@Deprecated
+	protected IssueTimeRange() { }
+
+	public IssueTimeRange(User user, Issue oldIssue, Issue newIssue, long issueHistoryID)
+	{
+	}
+
+	/**
+	 * @return Returns the organisationID.
+	 */
+	public String getOrganisationID() {
+		return organisationID;
+	}
+
+	/**
+	 * @return Returns the issueID.
+	 */
+	public long getIssueID() {
+		return issueID;
+	}
+
+	public long getIssueTimeRangeID() {
+		return issueTimeRangeID;
+	}
+
+	public Issue getIssue(){
+		return issue;
+	}
+
+	public Date getCreateTimestamp() {
+		return createTimestamp;
+	}
+	
+	public String getChange() {
+		return change;
+	}
+	
+	/**
+	 * Internal method.
+	 * @return The PersistenceManager associated with this object. 
+	 */
+	protected PersistenceManager getPersistenceManager() {
+		PersistenceManager issueTimeRangePM = JDOHelper.getPersistenceManager(this);
+		if (issueTimeRangePM == null)
+			throw new IllegalStateException("This instance of " + this.getClass().getName() + " is not persistent, can not get a PersistenceManager!");
+
+		return issueTimeRangePM;
+	}	
+	
+	@Override
+	public boolean equals(Object obj)
+	{
+		if (obj == this) return true;
+		if (!(obj instanceof IssueTimeRange)) return false;
+		IssueTimeRange o = (IssueTimeRange) obj;
+		return
+			Util.equals(this.organisationID, o.organisationID) && 
+			Util.equals(this.issueID, o.issueID) &&
+			Util.equals(this.issueTimeRangeID, o.issueTimeRangeID);
+	}
+
+	@Override
+	public int hashCode()
+	{
+		return 
+			Util.hashCode(organisationID) ^
+			Util.hashCode(issueID) ^
+			Util.hashCode(issueTimeRangeID);
+	}
+}

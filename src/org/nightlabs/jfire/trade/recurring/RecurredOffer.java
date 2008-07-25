414147bd6f524227b33a6b939c26b311edf4c00d
fitas_java <fitas_java@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-07-25 11:44:51 +0000

    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireTrade@11513 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/trade/recurring/RecurredOffer.java
+++ src/org/nightlabs/jfire/trade/recurring/RecurredOffer.java
@@ -1,6 +1,10 @@
 package org.nightlabs.jfire.trade.recurring;
 
+import org.nightlabs.jfire.security.User;
 import org.nightlabs.jfire.trade.Offer;
+import org.nightlabs.jfire.trade.Order;
+
+
 /**
  * @author Fitas Amine <fitas@nightlabs.de>
  * @jdo.persistence-capable
@@ -18,7 +22,23 @@ import org.nightlabs.jfire.trade.Offer;
  */
 public class RecurredOffer extends Offer{
 
+	/**
+	 * @jdo.field persistence-modifier="persistent"
+	 */
 	private RecurringOffer recurringOffer;
+
+	/**
+	 * @deprecated Only for JDO!
+	 */
+	@Deprecated
+	protected RecurredOffer() {}
+
+	public RecurredOffer(User user, Order order, String offerIDPrefix,
+			long offerID) {
+		super(user, order, offerIDPrefix, offerID);
+
+	}
+
 
 
 }

6295928b4ab4a42a6449816ac32788d96cd92d9f
fitas_java <fitas_java@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-08-01 10:41:32 +0000

    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireSimpleTrade@11604 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/simpletrade/store/recurring/SimpleRecurringTradeProductTypeActionHandler.java
+++ src/org/nightlabs/jfire/simpletrade/store/recurring/SimpleRecurringTradeProductTypeActionHandler.java
@@ -1,10 +1,23 @@
 package org.nightlabs.jfire.simpletrade.store.recurring;
 
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Collection;
 import java.util.Set;
 
+import javax.jdo.PersistenceManager;
+
+import org.nightlabs.jfire.security.SecurityReflector;
+import org.nightlabs.jfire.security.User;
 import org.nightlabs.jfire.simpletrade.store.SimpleProductType;
+import org.nightlabs.jfire.store.Product;
+import org.nightlabs.jfire.store.ProductType;
+import org.nightlabs.jfire.store.Store;
 import org.nightlabs.jfire.trade.Article;
+import org.nightlabs.jfire.trade.ArticleCreator;
+import org.nightlabs.jfire.trade.Segment;
+import org.nightlabs.jfire.trade.SegmentType;
+import org.nightlabs.jfire.trade.Trader;
 import org.nightlabs.jfire.trade.recurring.RecurredOffer;
 import org.nightlabs.jfire.trade.recurring.RecurringTradeProductTypeActionHandler;
 
@@ -43,7 +56,43 @@ extends RecurringTradeProductTypeActionHandler{
 	@Override
 	public  Map<Article, Article> createArticles(RecurredOffer offer, Set<Article> recurringArticles)
 	{	
-		return null;
+//		Map<Article, Article> map;
+//		Article article;
+//		
+//		PersistenceManager pm = getPersistenceManager();
+//		
+//	    Trader trader = Trader.getTrader(pm);
+//		
+//	    Store store = Store.getStore(pm);
+//		
+//		User user = SecurityReflector.getUserDescriptor().getUser(pm);
+//		
+//		ProductType pt;
+//		
+//		SegmentType segmentType = SegmentType.getDefaultSegmentType(pm);
+//		Segment segment = trader.createSegment(offer.getOrder(), segmentType);
+//		
+//		for (Iterator<Article> it = recurringArticles.iterator(); it.hasNext(); ) 
+//		{	
+//			article = it.next();
+//		    pt = article.getProductType();
+//		
+//		}
+//	
+//		SimpleProductType productType = (SimpleProductType)pt;		
+//		
+//		// find  Products
+//		Collection<? extends Product> products = store.findProducts(user, productType, null, null); // we create exactly one => no NestedProductTypeLocal needed
+//		if (products.size() != 1)
+//			throw new IllegalStateException("store.findProducts(...) created " + products.size() + " instead of exactly 1 product!");
+//		
+//		Collection<? extends Article> articles = trader.createArticles(user, offer, segment, products,
+//				new ArticleCreator(null), true, false);
+//
+//
+//		
+//		return (Map<Article, Article>) articles;
+	    return null;
 	}
 
 

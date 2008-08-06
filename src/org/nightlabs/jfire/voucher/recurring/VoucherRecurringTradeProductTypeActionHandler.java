d0b4b60708ef99ecb9480eea57ef1557efdab5ed
fitas_java <fitas_java@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-08-06 06:43:48 +0000

    added voucherhandlerproducttype class
    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireVoucher@11655 18c61ebf-f106-0410-9e1e-81fb82415b8f

---
+++ src/org/nightlabs/jfire/voucher/recurring/VoucherRecurringTradeProductTypeActionHandler.java
@@ -0,0 +1,108 @@
+package org.nightlabs.jfire.voucher.recurring;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+import javax.jdo.PersistenceManager;
+
+import org.nightlabs.ModuleException;
+import org.nightlabs.jfire.security.SecurityReflector;
+import org.nightlabs.jfire.security.User;
+import org.nightlabs.jfire.store.Product;
+import org.nightlabs.jfire.store.ProductType;
+import org.nightlabs.jfire.store.Store;
+import org.nightlabs.jfire.trade.Article;
+import org.nightlabs.jfire.trade.ArticleCreator;
+import org.nightlabs.jfire.trade.Segment;
+import org.nightlabs.jfire.trade.Trader;
+import org.nightlabs.jfire.trade.recurring.RecurredOffer;
+import org.nightlabs.jfire.trade.recurring.RecurringTradeProductTypeActionHandler;
+import org.nightlabs.jfire.voucher.store.VoucherType;
+
+/**
+ * @author Fitas Amine - fitas at nightlabs dot de
+ *
+ * @jdo.persistence-capable
+ *		identity-type="application"
+ *		persistence-capable-superclass="org.nightlabs.jfire.trade.recurring.RecurringTradeProductTypeActionHandler"
+ *		detachable="true"
+ *		table="JFireSimpleTrade_VoucherRecurringTradeProductTypeActionHandler"
+ *
+ * @jdo.inheritance strategy="superclass-table"
+ */
+
+public class VoucherRecurringTradeProductTypeActionHandler 
+extends	RecurringTradeProductTypeActionHandler
+{
+
+	
+	/**
+	 * @see ProductTypeActionHandler#ProductTypeActionHandler(String, String, Class)
+	 */
+	public VoucherRecurringTradeProductTypeActionHandler(String organisationID,
+			String productTypeActionHandlerID, Class<? extends VoucherType> productTypeClass)
+	{
+		super(organisationID, productTypeActionHandlerID, productTypeClass);
+	}
+	
+	/**
+	 * @deprecated Only for JDO!
+	 */
+	@Deprecated
+	protected VoucherRecurringTradeProductTypeActionHandler() { }
+
+
+
+	@Override
+	public  Map<Article, Article> createArticles(RecurredOffer offer, Set<Article> recurringArticles,Segment segment)
+	{	
+		Map<Article, Article> articlesMap=  new HashMap<Article, Article>();
+
+		Article article;
+
+		PersistenceManager pm = getPersistenceManager();
+		Trader trader = Trader.getTrader(pm);
+		Store store = Store.getStore(pm);
+		User user = SecurityReflector.getUserDescriptor().getUser(pm);
+
+		ProductType pt = null;
+		Article articleCreated =null;
+
+		for (Iterator<Article> it = recurringArticles.iterator(); it.hasNext(); ) 
+		{	
+			article = it.next();
+			pt = article.getProductType();
+
+			VoucherType productType = (VoucherType)pt;		
+
+			// find  Products
+			Collection<? extends Product> products = store.findProducts(user, productType, null, null); // we create exactly one => no NestedProductTypeLocal needed
+			if (products.size() != 1)
+				throw new IllegalStateException("store.findProducts(...) created " + products.size() + " instead of exactly 1 product!");
+
+			try {
+				ArrayList<? extends Article> articles=  (ArrayList<? extends Article>) trader.createArticles(user, offer, segment, products,
+						new ArticleCreator(null), true, false);
+				if (articles.size() != 1)
+					throw new IllegalStateException("store.findProducts(...) created " + products.size() + " instead of exactly 1 product!");
+				articleCreated = articles.get(0);
+			} catch (ModuleException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			articlesMap.put(article,articleCreated);
+
+		}
+
+		return articlesMap;
+	}
+
+
+	
+	
+}

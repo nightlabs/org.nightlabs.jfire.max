7a4a5c16fd5dd0316b0b271be7a1c60b7d8e4928
alex <alex@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-08-18 11:13:48 +0000

    started fixes for recurring trade using test case
    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireTrade@11862 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/trade/recurring/RecurringOrder.java
+++ src/org/nightlabs/jfire/trade/recurring/RecurringOrder.java
@@ -13,7 +13,7 @@ import org.nightlabs.jfire.trade.Order;
  *		identity-type="application"
  *		persistence-capable-superclass="org.nightlabs.jfire.trade.Order"
  *		detachable="true"
- *		table="JFireTrade_Recurring"
+ *		table="JFireTrade_RecurringOrder"
  *
  * @jdo.inheritance strategy="new-table"
  * 

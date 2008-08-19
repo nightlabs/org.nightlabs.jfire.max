2a92e0b28acf24fc8a48906f107922599ffafe7c
chairatk <chairatk@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-08-19 06:25:51 +0000

    brought IssueType things back.
    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireIssueTracking@11866 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/issue/dao/IssuePriorityDAO.java
+++ src/org/nightlabs/jfire/issue/dao/IssuePriorityDAO.java
@@ -1,13 +1,16 @@
 package org.nightlabs.jfire.issue.dao;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Set;
 
+import org.nightlabs.jdo.NLJDOHelper;
 import org.nightlabs.jfire.base.jdo.BaseJDOObjectDAO;
 import org.nightlabs.jfire.issue.IssueManager;
 import org.nightlabs.jfire.issue.IssueManagerUtil;
 import org.nightlabs.jfire.issue.IssuePriority;
+import org.nightlabs.jfire.issue.IssueResolution;
 import org.nightlabs.jfire.issue.id.IssuePriorityID;
 import org.nightlabs.jfire.security.SecurityReflector;
 import org.nightlabs.progress.ProgressMonitor;
@@ -64,15 +67,9 @@ extends BaseJDOObjectDAO<IssuePriorityID, IssuePriority>
 	public synchronized List<IssuePriority> getIssuePriorities(String[] fetchGroups, int maxFetchDepth, ProgressMonitor monitor)
 	{
 		try {
-			issueManager = IssueManagerUtil.getHome(SecurityReflector.getInitialContextProperties()).create();
-			try {
-				Set<IssuePriorityID> issuePriorityIDs = issueManager.getIssuePriorityIDs();
-				return getJDOObjects(null, issuePriorityIDs, fetchGroups, maxFetchDepth, monitor);
-			} finally {
-				issueManager = null;
-			}
+			return new ArrayList<IssuePriority>(retrieveJDOObjects(null, fetchGroups, NLJDOHelper.MAX_FETCH_DEPTH_NO_LIMIT, monitor));
 		} catch (Exception e) {
-			throw new RuntimeException(e);
+			throw new RuntimeException("Error while fetching issue resolutions: " + e.getMessage(), e); //$NON-NLS-1$
 		} 
 	}
 

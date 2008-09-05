97477f9e3faec01405c30c2d56bc7932ec77cdf1
chairatk <chairatk@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-09-05 05:07:17 +0000

    added test projects
    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireIssueTracking@12011 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/issue/IssueManagerBean.java
+++ src/org/nightlabs/jfire/issue/IssueManagerBean.java
@@ -1061,44 +1061,56 @@ implements SessionBean
 			issueLinkType.addLinkedObjectClass(Issue.class);
 			issueLinkType = pm.makePersistent(issueLinkType);
 			
-//			// Create the projects
-//			Project project;
-//
-//			project = new Project(IDGenerator.getOrganisationID(), IDGenerator.nextID(Project.class));
-//			project.getName().setText(Locale.ENGLISH.getLanguage(), "Project 1");
-//			project = pm.makePersistent(project);
-//
-//			project = new Project(IDGenerator.getOrganisationID(), IDGenerator.nextID(Project.class));
-//			project.getName().setText(Locale.ENGLISH.getLanguage(), "Project 2");
-//			project = pm.makePersistent(project);
-//			
-//			project = new Project(IDGenerator.getOrganisationID(), IDGenerator.nextID(Project.class));
-//			project.getName().setText(Locale.ENGLISH.getLanguage(), "Project 2");
-//			project = pm.makePersistent(project);
-//			
-//			project = new Project(IDGenerator.getOrganisationID(), IDGenerator.nextID(Project.class));
-//			project.getName().setText(Locale.ENGLISH.getLanguage(), "Project 3");
-//			project = pm.makePersistent(project);
-//			
-//			project = new Project(IDGenerator.getOrganisationID(), IDGenerator.nextID(Project.class));
-//			project.getName().setText(Locale.ENGLISH.getLanguage(), "Project 4");
-//			project = pm.makePersistent(project);
-//			
-//			Project subProject = new Project(IDGenerator.getOrganisationID(), IDGenerator.nextID(Project.class));
-//			subProject.getName().setText(Locale.ENGLISH.getLanguage(), "Sub project 1");
-//			project.addSubProject(subProject);
-//			
-//			subProject = new Project(IDGenerator.getOrganisationID(), IDGenerator.nextID(Project.class));
-//			subProject.getName().setText(Locale.ENGLISH.getLanguage(), "Sub project 2");
-//			project.addSubProject(subProject);
-//			
-//			subProject = new Project(IDGenerator.getOrganisationID(), IDGenerator.nextID(Project.class));
-//			subProject.getName().setText(Locale.ENGLISH.getLanguage(), "Sub project 3");
-//			project.addSubProject(subProject);
-//			
-//			subProject = new Project(IDGenerator.getOrganisationID(), IDGenerator.nextID(Project.class));
-//			subProject.getName().setText(Locale.ENGLISH.getLanguage(), "Sub project 4");
-//			project.addSubProject(subProject);
+			// Create the projects
+			Project project;
+
+			project = new Project(IDGenerator.getOrganisationID(), IDGenerator.nextID(Project.class));
+			project.getName().setText(Locale.ENGLISH.getLanguage(), "Project 1");
+			project = pm.makePersistent(project);
+			
+			Project subProject = new Project(IDGenerator.getOrganisationID(), IDGenerator.nextID(Project.class));
+			subProject.getName().setText(Locale.ENGLISH.getLanguage(), "Sub project 1");
+			project.addSubProject(subProject);
+			
+			Project subsubProject = new Project(IDGenerator.getOrganisationID(), IDGenerator.nextID(Project.class));
+			subsubProject.getName().setText(Locale.ENGLISH.getLanguage(), "Sub Sub project 1");
+			subProject.addSubProject(subsubProject);
+			
+			subsubProject = new Project(IDGenerator.getOrganisationID(), IDGenerator.nextID(Project.class));
+			subsubProject.getName().setText(Locale.ENGLISH.getLanguage(), "Sub Sub project 2");
+			subProject.addSubProject(subsubProject);
+			
+			subsubProject = new Project(IDGenerator.getOrganisationID(), IDGenerator.nextID(Project.class));
+			subsubProject.getName().setText(Locale.ENGLISH.getLanguage(), "Sub Sub project 3");
+			subProject.addSubProject(subsubProject);
+			
+			subProject = new Project(IDGenerator.getOrganisationID(), IDGenerator.nextID(Project.class));
+			subProject.getName().setText(Locale.ENGLISH.getLanguage(), "Sub project 2");
+			project.addSubProject(subProject);
+			
+			subProject = new Project(IDGenerator.getOrganisationID(), IDGenerator.nextID(Project.class));
+			subProject.getName().setText(Locale.ENGLISH.getLanguage(), "Sub project 3");
+			project.addSubProject(subProject);
+			
+			subProject = new Project(IDGenerator.getOrganisationID(), IDGenerator.nextID(Project.class));
+			subProject.getName().setText(Locale.ENGLISH.getLanguage(), "Sub project 4");
+			project.addSubProject(subProject);
+
+			project = new Project(IDGenerator.getOrganisationID(), IDGenerator.nextID(Project.class));
+			project.getName().setText(Locale.ENGLISH.getLanguage(), "Project 2");
+			project = pm.makePersistent(project);
+			
+			project = new Project(IDGenerator.getOrganisationID(), IDGenerator.nextID(Project.class));
+			project.getName().setText(Locale.ENGLISH.getLanguage(), "Project 3");
+			project = pm.makePersistent(project);
+			
+			project = new Project(IDGenerator.getOrganisationID(), IDGenerator.nextID(Project.class));
+			project.getName().setText(Locale.ENGLISH.getLanguage(), "Project 4");
+			project = pm.makePersistent(project);
+			
+			project = new Project(IDGenerator.getOrganisationID(), IDGenerator.nextID(Project.class));
+			project.getName().setText(Locale.ENGLISH.getLanguage(), "Project 5");
+			project = pm.makePersistent(project);
 			
 			EditLockType issueEditLock = new EditLockType(EditLockTypeIssue.EDIT_LOCK_TYPE_ID);
 			issueEditLock = pm.makePersistent(issueEditLock);

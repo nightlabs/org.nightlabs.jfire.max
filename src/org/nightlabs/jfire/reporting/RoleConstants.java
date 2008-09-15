b28174a3a291cca5c8a1572c1b16926a9407fa18
marco <marco@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-09-15 14:59:13 +0000

    added RoleConstants
    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireReporting@12086 18c61ebf-f106-0410-9e1e-81fb82415b8f

---
+++ src/org/nightlabs/jfire/reporting/RoleConstants.java
@@ -0,0 +1,8 @@
+package org.nightlabs.jfire.reporting;
+
+import org.nightlabs.jfire.security.id.RoleID;
+
+public class RoleConstants {
+	public static final RoleID editReport = RoleID.create("org.nightlabs.jfire.reporting.editReport");
+	public static final RoleID renderReport = RoleID.create("org.nightlabs.jfire.reporting.renderReport");
+}

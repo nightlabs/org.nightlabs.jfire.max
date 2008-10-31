05e2aacd2de4f29986757fb06705a5c715f66913
nozkiller <nozkiller@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-10-31 17:13:08 +0000

    changed content type to application/x-....
    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireVoucherEditor2D@12565 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/voucher/editor2d/iofilter/VoucherXStreamFilter.java
+++ src/org/nightlabs/jfire/voucher/editor2d/iofilter/VoucherXStreamFilter.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package org.nightlabs.jfire.voucher.editor2d.iofilter;
 
@@ -10,12 +10,12 @@ import org.nightlabs.editor2d.iofilter.XStreamFilter;
  * @author Daniel Mazurek - daniel [at] nightlabs [dot] de
  *
  */
-public class VoucherXStreamFilter 
-extends XStreamFilter 
+public class VoucherXStreamFilter
+extends XStreamFilter
 {
-	public static final String FILE_EXTENSION = "v2d"; //$NON-NLS-1$	
-	public static final String VOUCHER_CONTENT_TYPE = "vnd.nightlabs.jfire.voucher"; //$NON-NLS-1$
-	
+	public static final String FILE_EXTENSION = "v2d"; //$NON-NLS-1$
+	public static final String VOUCHER_CONTENT_TYPE = "application/x-nightlabs-jfire-voucher"; //$NON-NLS-1$
+
 	@Override
 	protected String initDescription() {
 		return "JFire Voucher Design File Format";
@@ -37,5 +37,5 @@ extends XStreamFilter
 		manifestWriter.setContentType(VOUCHER_CONTENT_TYPE);
 		return manifestWriter;
 	}
-	
+
 }

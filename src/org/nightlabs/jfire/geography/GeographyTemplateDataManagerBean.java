2d345f9733e38debafb7f1f5ba370b9ae7e18ad8
marco <marco@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2009-03-12 16:11:13 +0000

    switched version number from 0.9.5 to 0.9.7 (forgot to set it to 0.9.6 for the release)
    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireGeography@13609 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/geography/GeographyTemplateDataManagerBean.java
+++ src/org/nightlabs/jfire/geography/GeographyTemplateDataManagerBean.java
@@ -246,7 +246,7 @@ implements SessionBean
 
 			// version is {major}.{minor}.{release}-{patchlevel}-{suffix}
 			moduleMetaData = new ModuleMetaData(
-					JFireGeographyEAR.MODULE_NAME, "0.9.5-0-beta", "0.9.5-0-beta");
+					JFireGeographyEAR.MODULE_NAME, "0.9.7-0-beta", "0.9.7-0-beta");
 			pm.makePersistent(moduleMetaData);
 
 

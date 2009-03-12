47b19f81a49c064d6d67795fc702e96846218542
marco <marco@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2009-03-12 16:11:13 +0000

    switched version number from 0.9.5 to 0.9.7 (forgot to set it to 0.9.6 for the release)
    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireTrade@13609 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/trade/TradeManagerBean.java
+++ src/org/nightlabs/jfire/trade/TradeManagerBean.java
@@ -1367,7 +1367,7 @@ implements SessionBean
 
 			// version is {major}.{minor}.{release}.{patchlevel}.{suffix}
 			moduleMetaData = new ModuleMetaData(
-					JFireTradeEAR.MODULE_NAME, "0.9.5.0.beta", "0.9.5.0.beta");
+					JFireTradeEAR.MODULE_NAME, "0.9.7.0.beta", "0.9.7.0.beta");
 			pm.makePersistent(moduleMetaData);
 
 			ConfigSetup userConfigSetup = ConfigSetup.getConfigSetup(

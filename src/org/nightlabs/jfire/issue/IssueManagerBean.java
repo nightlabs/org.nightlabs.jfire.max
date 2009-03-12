58ad4cec3d9336bb622a6c60cda8ed38fb3b71ef
marco <marco@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2009-03-12 16:11:13 +0000

    switched version number from 0.9.5 to 0.9.7 (forgot to set it to 0.9.6 for the release)
    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireIssueTracking@13609 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/issue/IssueManagerBean.java
+++ src/org/nightlabs/jfire/issue/IssueManagerBean.java
@@ -1253,7 +1253,7 @@ implements SessionBean
 			logger.info("Initialization of " + JFireIssueTrackingEAR.MODULE_NAME + " started...");
 
 			pm.makePersistent(new ModuleMetaData(
-					JFireIssueTrackingEAR.MODULE_NAME, "0.9.5.0.0.beta", "0.9.5.0.0.beta")
+					JFireIssueTrackingEAR.MODULE_NAME, "0.9.7.0.0.beta", "0.9.7.0.0.beta")
 			);
 
 			IssueType issueType = new IssueType(getOrganisationID(), IssueType.DEFAULT_ISSUE_TYPE_ID);

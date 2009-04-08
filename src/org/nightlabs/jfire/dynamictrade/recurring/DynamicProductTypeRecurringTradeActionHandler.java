5dba8e50c1f4074073580c00e8b624680fd4fdf4
fitas_java <fitas_java@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2009-04-08 15:49:11 +0000

    treatment of javascript names upon the creation of new recurred articles
    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireDynamicTrade@13828 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/dynamictrade/recurring/DynamicProductTypeRecurringTradeActionHandler.java
+++ src/org/nightlabs/jfire/dynamictrade/recurring/DynamicProductTypeRecurringTradeActionHandler.java
@@ -7,7 +7,9 @@ import java.util.Set;
 
 import javax.jdo.JDOHelper;
 import javax.jdo.PersistenceManager;
+import javax.script.ScriptException;
 
+import org.nightlabs.i18n.I18nText;
 import org.nightlabs.jfire.accounting.id.TariffID;
 import org.nightlabs.jfire.dynamictrade.DynamicTrader;
 import org.nightlabs.jfire.dynamictrade.store.DynamicProductType;
@@ -19,6 +21,7 @@ import org.nightlabs.jfire.trade.id.OfferID;
 import org.nightlabs.jfire.trade.id.SegmentID;
 import org.nightlabs.jfire.trade.recurring.RecurredOffer;
 import org.nightlabs.jfire.trade.recurring.RecurringTradeProductTypeActionHandler;
+import org.nightlabs.util.ScriptParser;
 
 /**
  * @jdo.persistence-capable
@@ -54,26 +57,36 @@ public class DynamicProductTypeRecurringTradeActionHandler extends RecurringTrad
 	@Override
 	public Map<Article, Article> createArticles(RecurredOffer offer,
 			Set<Article> recurringArticles, Segment segment)
-	{
+			{
 		Map<Article, Article> articlesMap=  new HashMap<Article, Article>();
 
 		DynamicProductTypeRecurringArticle recurringArticle;
 
 		PersistenceManager pm = getPersistenceManager();
 		DynamicTrader dtm = DynamicTrader.getDynamicTrader(pm);
-
+		ScriptParser script = new ScriptParser();
+		
 		for (Iterator<Article> it = recurringArticles.iterator(); it.hasNext(); ) 
 		{	
 			recurringArticle = (DynamicProductTypeRecurringArticle)it.next();
 
+
 			ProductTypeID productTypeID = (ProductTypeID)JDOHelper.getObjectId(recurringArticle.getProductType());		
 			SegmentID segmentID = (SegmentID) JDOHelper.getObjectId(segment);
-			TariffID tariffID = (TariffID) JDOHelper.getObjectId(recurringArticle.getTariff());
-
-
-			Article article = dtm.createArticle(segmentID, (OfferID) JDOHelper.getObjectId(offer), productTypeID, recurringArticle.getQuantity(), recurringArticle.getUnitID(), 
-					tariffID, recurringArticle.getName(), 
+			TariffID tariffID = (TariffID) JDOHelper.getObjectId(recurringArticle.getTariff());				
+			I18nText name = null; 			
+			Article article;
+			// sets the in the Script Engine.
+			script.getTextScriptContent().copyFrom(recurringArticle.getName());	
+			try {
+				name = script.execute();	
+			} catch (ScriptException e) {				
+				throw new IllegalStateException("Wrong script code in the Article name " + e.getMessage());
+			}
+			article = dtm.createArticle(segmentID, (OfferID) JDOHelper.getObjectId(offer), productTypeID, recurringArticle.getQuantity(), recurringArticle.getUnitID(), 
+					tariffID, name, 
 					recurringArticle.getSinglePrice(),  true, true);
+
 			if (article == null)
 				throw new IllegalStateException("trader.createArticles(...) created  instead of exactly 1 article!");
 
@@ -84,6 +97,6 @@ public class DynamicProductTypeRecurringTradeActionHandler extends RecurringTrad
 
 		return articlesMap;
 
-	}
+			}
 
 }

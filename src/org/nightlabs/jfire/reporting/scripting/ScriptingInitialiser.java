f7951e0bab2248b6002a917812e6141f1453ad61
marco <marco@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2009-05-03 12:15:33 +0000

    * fixed classpath (BIRT-libs are now in target-...-birt-project). * Changed ScriptingInitialiser to use temporarily extracted EAR-directory
    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireReporting@14390 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/reporting/scripting/ScriptingInitialiser.java
+++ src/org/nightlabs/jfire/reporting/scripting/ScriptingInitialiser.java
@@ -13,17 +13,17 @@ import org.nightlabs.jfire.servermanager.JFireServerManager;
 
 /**
  * Initialises the scripts for the trade reporting module.
- * 
+ *
  * @author Alexander Bieber <alex [AT] nightlabs [DOT] de>
  *
  */
 public class ScriptingInitialiser {
-	
+
 	/**
 	 * Uses a {@link org.nightlabs.jfire.scripting.ScriptingInitialiser} to add
 	 * all scripts in the script subdirectory of the JFireReportingTrade.ear.
 	 * It will create a root category for all scripts
-	 * 
+	 *
 	 * @param pm The PersistenceManager to use.
 	 * @param jfireServerManager The ServerManager to use.
 	 * @param organisationID The organisationID the stored scirpts will have.
@@ -43,7 +43,7 @@ public class ScriptingInitialiser {
 				ScriptingConstants.SCRIPT_REGISTRY_ITEM_ID_CATEGORY_ROOT);
 		rootCategory.getName().setText(Locale.ENGLISH.getLanguage(), "Reporting");
 		rootCategory.getName().setText(Locale.GERMAN.getLanguage(), "Reporting");
-		
+
 		ScriptCategory baseCategory = org.nightlabs.jfire.scripting.ScriptingInitialiser.createCategory(
 				pm, rootCategory,
 				organisationID,
@@ -51,18 +51,19 @@ public class ScriptingInitialiser {
 				ScriptingConstants.SCRIPT_REGISTRY_ITEM_ID_CATEGORY_BASE);
 		baseCategory.getName().setText(Locale.ENGLISH.getLanguage(), "Base scripts");
 		baseCategory.getName().setText(Locale.GERMAN.getLanguage(), "Base scripts");
-		
+
 		String j2eeBaseDir = jfireServerManager.getJFireServerConfigModule().getJ2ee().getJ2eeDeployBaseDirectory();
-		String scriptDirSuffix = JFireReportingEAR.MODULE_NAME+".ear"+File.separator+"script";
+		File ear = new File(j2eeBaseDir, JFireReportingEAR.MODULE_NAME + ".ear");
+		String scriptDirSuffix = "script";
 		File scriptDir = new File(j2eeBaseDir, scriptDirSuffix);
 		File[] subDirs = scriptDir.listFiles();
 		for (int i = 0; i < subDirs.length; i++) {
 			if (subDirs[i].isDirectory()) {
 				new org.nightlabs.jfire.scripting.ScriptingInitialiser(
+						ear,
 						scriptDirSuffix+File.separator+subDirs[i].getName(),
 						baseCategory,
 						ScriptingConstants.SCRIPT_REGISTRY_ITEM_ID_CATEGORY_BASE,
-						jfireServerManager,
 						pm,
 						organisationID
 					).initialise();

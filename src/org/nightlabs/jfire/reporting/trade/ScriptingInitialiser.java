3d941d9356bbcb5007470b90c95abb5e3ddf2afc
marco <marco@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2009-05-03 12:15:33 +0000

    * fixed classpath (BIRT-libs are now in target-...-birt-project). * Changed ScriptingInitialiser to use temporarily extracted EAR-directory
    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireReportingTrade@14390 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/reporting/trade/ScriptingInitialiser.java
+++ src/org/nightlabs/jfire/reporting/trade/ScriptingInitialiser.java
@@ -15,17 +15,17 @@ import org.nightlabs.jfire.servermanager.JFireServerManager;
 
 /**
  * Initializes the scripts for the trade reporting module.
- * 
+ *
  * @author Alexander Bieber <alex [AT] nightlabs [DOT] de>
  *
  */
 public class ScriptingInitialiser {
-	
+
 	/**
 	 * Uses a {@link org.nightlabs.jfire.scripting.ScriptingInitialiser} to add
 	 * all scripts in the script subdirectory of the JFireReportingTrade.ear.
 	 * It will create a root category for all scripts
-	 * 
+	 *
 	 * @param pm The PersistenceManager to use.
 	 * @param jfireServerManager The ServerManager to use.
 	 * @param organisationID The organisationID the stored scirpts will have.
@@ -41,14 +41,14 @@ public class ScriptingInitialiser {
 		ScriptRegistryItemID rootCatID = ScriptRegistryItemID.create(
 				organisationID, ScriptingConstants.SCRIPT_REGISTRY_ITEM_TYPE_ROOT,
 				ScriptingConstants.SCRIPT_REGISTRY_ITEM_ID_CATEGORY_ROOT);
-		
+
 		ScriptCategory rootCategory = null;
 		try {
 			rootCategory = (ScriptCategory) pm.getObjectById(rootCatID);
 		} catch (JDOObjectNotFoundException e) {
 			throw new IllegalStateException("Could not find root Reporting ScriptCategory. Was it intialized correctly?", e);
 		}
-		
+
 		ScriptCategory tradeCategory = org.nightlabs.jfire.scripting.ScriptingInitialiser.createCategory(
 				pm, rootCategory,
 				organisationID,
@@ -56,19 +56,20 @@ public class ScriptingInitialiser {
 				ScriptingTradeConstants.SCRIPT_REGISTRY_ITEM_ID_CATEGORY_TRADE);
 		tradeCategory.getName().setText(Locale.ENGLISH.getLanguage(), "Trade scripts");
 		tradeCategory.getName().setText(Locale.GERMAN.getLanguage(), "Trade scripts");
-		
-		
+
+
 		String j2eeBaseDir = jfireServerManager.getJFireServerConfigModule().getJ2ee().getJ2eeDeployBaseDirectory();
-		String scriptDirSuffix = JFireReportingTradeEAR.MODULE_NAME+".ear"+File.separator+"script";
+		File ear = new File(j2eeBaseDir, JFireReportingTradeEAR.MODULE_NAME + ".ear");
+		String scriptDirSuffix = "script";
 		File scriptDir = new File(j2eeBaseDir, scriptDirSuffix);
 		File[] subDirs = scriptDir.listFiles();
 		for (int i = 0; i < subDirs.length; i++) {
 			if (subDirs[i].isDirectory()) {
 				new org.nightlabs.jfire.scripting.ScriptingInitialiser(
+						ear,
 						scriptDirSuffix+File.separator+subDirs[i].getName(),
 						tradeCategory,
 						ScriptingTradeConstants.SCRIPT_REGISTRY_ITEM_TYPE_SCRIPT_TRADE,
-						jfireServerManager,
 						pm,
 						organisationID
 					).initialise();

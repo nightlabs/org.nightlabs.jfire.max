801dcc545f76f52eb5d3d421ff253fa4b99c5c40
nozkiller <nozkiller@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2009-05-11 18:57:07 +0000

    introduced projects org.nightlabs.jfire.trade + org.nightlabs.jfire.trade.ui
    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireIssueTimeTracking@14531 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/issuetimetracking/IssueTimeTrackingManagerRemote.java
+++ src/org/nightlabs/jfire/issuetimetracking/IssueTimeTrackingManagerRemote.java
@@ -5,7 +5,10 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Set;
 
+import javax.annotation.security.RolesAllowed;
 import javax.ejb.Remote;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
 
 import org.nightlabs.jfire.issue.project.id.ProjectID;
 import org.nightlabs.jfire.issuetimetracking.id.ProjectCostID;
@@ -13,7 +16,7 @@ import org.nightlabs.jfire.issuetimetracking.id.ProjectCostID;
 @Remote
 public interface IssueTimeTrackingManagerRemote
 {
-
+	@RolesAllowed("_Guest_")
 	List<ProjectCost> getProjectCosts(Collection<ProjectCostID> projectCostIDs,
 			String[] fetchGroups, int maxFetchDepth);
 
@@ -24,14 +27,19 @@ public interface IssueTimeTrackingManagerRemote
 	 * @param fetchGroups the fetchGroups that used for specify fields to be detached from the datastore
 	 * @param maxFetchDepth specifies the number of level of the object to be fetched
 	 */
+	@TransactionAttribute(TransactionAttributeType.REQUIRED)
+	@RolesAllowed("_Guest_")
 	ProjectCost storeProjectCost(ProjectCost projectCost, boolean get,
 			String[] fetchGroups, int maxFetchDepth);
 
+	@RolesAllowed("_Guest_")
 	Set<ProjectCostID> getProjectCostIDsByProjectID(ProjectID projectID);
 
 	/**
 	 * @throws IOException While loading an icon from a local resource, this might happen and we don't care in the initialise method.
 	 */
+	@TransactionAttribute(TransactionAttributeType.REQUIRED)
+	@RolesAllowed("_System_")
 	void initialise() throws Exception;
 
 }

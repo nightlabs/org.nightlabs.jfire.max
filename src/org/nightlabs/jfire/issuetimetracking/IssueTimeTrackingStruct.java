1a5007299c1cb18234a54a24f4ada732f2631110
marco <marco@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2009-07-17 09:56:03 +0000

    * Fixed a bug that happened during initial creation of a new organisation due to subclasses of StructField not yet known to DataNucleus. * Cleaned up the code a little bit.
    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireIssueTimeTracking@15056 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/issuetimetracking/IssueTimeTrackingStruct.java
+++ src/org/nightlabs/jfire/issuetimetracking/IssueTimeTrackingStruct.java
@@ -1,21 +1,18 @@
 package org.nightlabs.jfire.issuetimetracking;
 
-import java.util.HashMap;
 import java.util.Locale;
-import java.util.Map;
 
 import javax.jdo.PersistenceManager;
 
 import org.apache.log4j.Logger;
 import org.nightlabs.jfire.base.DuplicateKeyException;
 import org.nightlabs.jfire.department.prop.DepartmentStructField;
-import org.nightlabs.jfire.department.prop.PropHelper;
 import org.nightlabs.jfire.issue.Issue;
 import org.nightlabs.jfire.organisation.Organisation;
 import org.nightlabs.jfire.prop.IStruct;
 import org.nightlabs.jfire.prop.Struct;
 import org.nightlabs.jfire.prop.StructBlock;
-import org.nightlabs.jfire.prop.exception.StructFieldNotFoundException;
+import org.nightlabs.jfire.prop.exception.StructBlockNotFoundException;
 import org.nightlabs.jfire.prop.id.StructBlockID;
 import org.nightlabs.jfire.prop.id.StructFieldID;
 
@@ -23,57 +20,51 @@ import org.nightlabs.jfire.prop.id.StructFieldID;
  * This {@link IssueTimeTrackingStruct} class provides methods for creating {@link Struct}.
  *
  * @author Chairat Kongarayawetchakun <!-- chairat [AT] nightlabs [DOT] de -->
- *
+ * @author marco schulze - marco at nightlabs dot de
  */
 public class IssueTimeTrackingStruct
 {
 	private static final Logger logger = Logger.getLogger(IssueTimeTrackingStruct.class);
 
 	public static IStruct getIssueTimeTrackingStruct(PersistenceManager pm) {
+		pm.getExtent(DepartmentStructField.class);
+
 		Struct issueStruct = null;
 		issueStruct = Struct.getStruct(DEV_ORGANISATION_ID, Issue.class, Struct.DEFAULT_SCOPE, pm);
 		try {
-			logger.debug("Found Struct............");
-			StructBlock departmentStructBlock = issueStruct.getStructBlock(DEPARTMENT_BLOCK);
-			try {
-				departmentStructBlock.getStructField(DEPARTMENT_FIELD);
-			} catch (StructFieldNotFoundException e) {
-				createDepartmentStructField(departmentStructBlock);
-			}
-		} catch (Exception e) {
+			issueStruct.getStructBlock(DEPARTMENT_BLOCK);
+		} catch (StructBlockNotFoundException x) {
 			createDepartmentStructBlock(issueStruct);
 		}
-
-		pm.makePersistent(issueStruct);
 
 		return issueStruct;
 	}
 
-	private static void createDepartmentStructBlock(IStruct issueStruct) {
-		logger.debug("Creating Struct Block.....................");
+	private static StructBlock createDepartmentStructBlock(IStruct issueStruct) {
+		if (logger.isDebugEnabled())
+			logger.debug("Creating Struct Block.....................");
 
-		Map<String, String> blockNameMap = new HashMap<String, String>();
-		blockNameMap.put(Locale.GERMAN.getLanguage(), "Abteilungen");
-		blockNameMap.put(Locale.ENGLISH.getLanguage(), "Department");
-
-		StructBlock structBlock = PropHelper.createStructBlock(issueStruct, DEPARTMENT_BLOCK, blockNameMap);
+		StructBlock structBlock = new StructBlock(issueStruct, DEPARTMENT_BLOCK);
 		structBlock.setUnique(true);
+		structBlock.getName().setText(Locale.GERMAN, "Abteilung");
+		structBlock.getName().setText(Locale.ENGLISH, "Department");
+
 		createDepartmentStructField(structBlock);
 		try {
 			issueStruct.addStructBlock(structBlock);
 		} catch (DuplicateKeyException e) {
 			throw new RuntimeException(e);
 		}
+		return structBlock;
 	}
 
 	private static void createDepartmentStructField(StructBlock issueStructBlock) {
-		logger.debug("Creating Struct Field.................");
+		if (logger.isDebugEnabled())
+			logger.debug("Creating Struct Field.................");
 
-		Map<String, String> fieldNameMap = new HashMap<String, String>();
-		fieldNameMap.put(Locale.GERMAN.getLanguage(), "Abteilungen");
-		fieldNameMap.put(Locale.ENGLISH.getLanguage(), "Department");
-
-		DepartmentStructField departmentStructField = PropHelper.createDepartmentField(issueStructBlock, DEPARTMENT_FIELD, fieldNameMap);
+		DepartmentStructField departmentStructField = new DepartmentStructField(issueStructBlock, DEPARTMENT_FIELD);
+		departmentStructField.getName().setText(Locale.GERMAN, "Abteilung");
+		departmentStructField.getName().setText(Locale.ENGLISH, "Department");
 		try {
 			issueStructBlock.addStructField(departmentStructField);
 		} catch (DuplicateKeyException e) {

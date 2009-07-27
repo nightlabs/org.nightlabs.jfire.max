5c4eb19fa845893ec005a816218f5889719a1745
nozkiller <nozkiller@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2009-07-27 08:40:23 +0000

    removed dependencys Update to Template fragments
    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireWebShopBase@15171 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/web/webshop/Util.java
+++ src/org/nightlabs/jfire/web/webshop/Util.java
@@ -1,5 +1,7 @@
 package org.nightlabs.jfire.web.webshop;
 
+import java.util.Locale;
+
 /**
  * @author Khaled - khaled[at]nightlabs[dot]de
  * @version $Version$ - $Date$
@@ -11,5 +13,42 @@ public class Util {
 		String regex = "^[^@]+@([0-9a-zA-Z\\-]+\\.)+[a-zA-Z]+$";
 		return email.matches(regex);
 	}
+
+	/**
+     * Convert a string based locale into a Locale Object
+     * Strings are formatted:
+     * language_contry_variant
+     *
+     **/
+    
+    public static Locale getLocaleFromString(String localeString) {
+        if (localeString == null) return null;
+        if (localeString.toLowerCase().equals("default")) return Locale.getDefault();
+        int languageIndex = localeString.indexOf('_');
+        if (languageIndex  == -1) return null;
+        int countryIndex = localeString.indexOf('_', languageIndex +1);
+        String country = null;
+        if (countryIndex  == -1) {
+            if (localeString.length() > languageIndex) {
+                country = localeString.substring(languageIndex +1, localeString.length());
+            } else {
+                return null;
+            }
+        }
+        int variantIndex = -1;
+        if (countryIndex != -1) countryIndex = localeString.indexOf('_', countryIndex +1);
+        String language = localeString.substring(0, languageIndex);
+        String variant = null;
+        if (variantIndex  != -1) {
+
+            variant = localeString.substring(variantIndex +1, localeString.length());
+        }
+        if (variant != null) {
+            return new Locale(language, country, variant);
+        } else {
+            return new Locale(language, country);
+        }
+    }
+	
 	
 }

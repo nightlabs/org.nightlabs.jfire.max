bc344a64927bd27fe95e680aa9cb1f0e4f254251
khaireel <khaireel@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2009-11-23 09:06:14 +0000

    Revised the "shortened message" in PriceCalculationException.getShortenedErrorMessage(), to properly display sentence with punctuation.
    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireTrade@16026 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/accounting/gridpriceconfig/PriceCalculationException.java
+++ src/org/nightlabs/jfire/accounting/gridpriceconfig/PriceCalculationException.java
@@ -26,6 +26,8 @@
 
 package org.nightlabs.jfire.accounting.gridpriceconfig;
 
+import java.util.StringTokenizer;
+
 
 
 /**
@@ -34,7 +36,7 @@ package org.nightlabs.jfire.accounting.gridpriceconfig;
 public class PriceCalculationException extends Exception
 {
 	private static final long serialVersionUID = 1L;
-	
+
 	private IAbsolutePriceCoordinate absolutePriceCoordinate;
 
 	public PriceCalculationException(IAbsolutePriceCoordinate absolutePriceCoordinate)
@@ -77,10 +79,10 @@ public class PriceCalculationException extends Exception
 	{
 		return absolutePriceCoordinate;
 	}
-	
-	private static final String MOZILLA_EXCEPTION_CLASS = "org.mozilla.javascript"; // mozilla javascript engine
-	
-	
+
+//	private static final String MOZILLA_EXCEPTION_CLASS = "org.mozilla.javascript"; // mozilla javascript engine
+
+
 	/**
 	 * This method trims down the exception error string message to return a meaningfull Error message.
 	 * the Mozilla Java script engine throws the message in the format of MozzilaClassNameException:TitleError:ErrorMessage(PluginClassName)
@@ -88,34 +90,52 @@ public class PriceCalculationException extends Exception
 	 *
 	 * @return Returns the ErrorMessage.
 	 */
-	public String getShortenedErrorMessage()
-	{
-		String  exceptionError = getMessage();
-		// detects if the exception error thrown is coming from the Mozilla javascript engine
-		int lastInd = 0;
-		int expInd = exceptionError.indexOf(MOZILLA_EXCEPTION_CLASS);
-		// get to the last occurance of the exception to remove the wrapped exceptions.
-		while(lastInd > -1){
-			lastInd = exceptionError.indexOf(MOZILLA_EXCEPTION_CLASS, expInd + MOZILLA_EXCEPTION_CLASS.length());
-			if(lastInd > -1)
-				expInd = lastInd;
+	public String getShortenedErrorMessage() {
+		String msg = getMessage();
+		StringTokenizer strTok = new StringTokenizer(msg, ":");
+		while (strTok.hasMoreTokens()) {
+			String shortMsg = strTok.nextToken();
+			if (shortMsg.contains("(org.nightlabs.jfire.accounting.")) {  //$NON-NLS-1$
+				msg = shortMsg.substring(0, shortMsg.indexOf('(')).trim();
+				if (msg.charAt(msg.length()-1) != '.') msg += ".";
+
+				break;
+			}
 		}
 
-		if(expInd > -1)
-		{
-			// remove all the wrapped exceptions
-			String eclErr = exceptionError.substring(expInd, exceptionError.length());			
-			String[] str = eclErr.split(":");
-			if(str.length >= 2)
-				if(str[str.length - 1].indexOf("(") > -1)
-					return  str[str.length - 1].substring(0, str[str.length - 1].indexOf("("));
-				else
-					return  str[str.length - 1];
-			else
-				return  eclErr; 
-		}
-		else
-			return getMessage();
+		return msg;
 	}
-	
+
+//	public String getShortenedErrorMessage()
+	// Exception
+//	org.mozilla.javascript.WrappedException: Wrapped java.lang.IndexOutOfBoundsException: There is no Currency registered in this PriceConfig with the currencyID CHF (org.nightlabs.jfire.accounting.gridpriceconfig.AbsolutePriceCoordinate[chezfrancois.jfire.org/CustomerGroup-default,chezfrancois.jfire.org/_gold_card_,EUR,chezfrancois.
+//	{
+//		String  exceptionError = getMessage();
+//		// detects if the exception error thrown is coming from the Mozilla javascript engine
+//		int lastInd = 0;
+//		int expInd = exceptionError.indexOf(MOZILLA_EXCEPTION_CLASS);
+//		// get to the last occurance of the exception to remove the wrapped exceptions.
+//		while(lastInd > -1){
+//			lastInd = exceptionError.indexOf(MOZILLA_EXCEPTION_CLASS, expInd + MOZILLA_EXCEPTION_CLASS.length());
+//			if(lastInd > -1)
+//				expInd = lastInd;
+//		}
+//
+//		if(expInd > -1)
+//		{
+//			// remove all the wrapped exceptions
+//			String eclErr = exceptionError.substring(expInd, exceptionError.length());
+//			String[] str = eclErr.split(":");
+//			if(str.length >= 2)
+//				if(str[str.length - 1].indexOf("(") > -1)
+//					return  str[str.length - 1].substring(0, str[str.length - 1].indexOf("("));
+//				else
+//					return  str[str.length - 1];
+//			else
+//				return  eclErr;
+//		}
+//		else
+//			return getMessage();
+//	}
+
 }

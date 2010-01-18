5aebf230b6b990dd45b1f1b16e3409ac65ff93c2
tobias <tobias@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2010-01-18 13:18:41 +0000

    Continued work on configurable search filter items. - StructFieldSearchFilterItemEditors can be registered through an structFieldSearchfilterItemEditor-extension point - DynamicPersonSearch now uses these new StructFieldSearchFilterItemEditors -
    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireReporting@16682 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/reporting/layout/ReportCategorySearchFilterItem.java
+++ src/org/nightlabs/jfire/reporting/layout/ReportCategorySearchFilterItem.java
@@ -26,9 +26,11 @@
 
 package org.nightlabs.jfire.reporting.layout;
 
+import java.util.EnumSet;
 import java.util.Map;
 import java.util.Set;
 
+import org.nightlabs.jdo.search.MatchType;
 import org.nightlabs.jdo.search.SearchFilterItem;
 
 /**
@@ -51,52 +53,35 @@ public class ReportCategorySearchFilterItem extends SearchFilterItem {
 	 * @param matchType
 	 * @param needle
 	 */
-	public ReportCategorySearchFilterItem(int matchType, String needle) {
-		super(matchType, needle);
+	public ReportCategorySearchFilterItem(MatchType matchType) {
+		super(matchType);
 	}
 
-	/* (non-Javadoc)
-	 * @see com.nightlabs.jdo.search.SearchFilterItem#getSearchField()
-	 */
-	@Override
-	public Object getSearchField() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	/* (non-Javadoc)
-	 * @see com.nightlabs.jdo.search.SearchFilterItem#isConstraint()
-	 */
-	@Override
-	public boolean isConstraint() {
-		return (nameFilter != null) || (internalFilter != INTERNAL_FILTER_BOTH);
-	}
-
-	/* (non-Javadoc)
-	 * @see com.nightlabs.jdo.search.SearchFilterItem#getItemTargetClass()
-	 */
-	@Override
-	public Class getItemTargetClass() {
-		return null;
-	}
-
-	/* (non-Javadoc)
-	 * @see com.nightlabs.jdo.search.SearchFilterItem#appendSubQuery(int, int, java.util.Set, java.lang.StringBuffer, java.lang.StringBuffer, java.lang.StringBuffer, java.util.Map)
-	 */
-	@Override
-	public void appendSubQuery(int itemIndex, int itemSubIndex, Set imports,
-			StringBuffer vars, StringBuffer filter, StringBuffer params,
-			Map paramMap) {
-		if (!isConstraint())
-			return;
-		
-		filter.append("(");
-		if (nameFilter != null)
-			filter.append("this.name.names.contains()");
-		// TODO: Howto search for name in map ...
-		
-		filter.append(")");
-	}
+//	/* (non-Javadoc)
+//	 * @see com.nightlabs.jdo.search.SearchFilterItem#getSearchField()
+//	 */
+//	@Override
+//	public Object getSearchField() {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+//
+//	/* (non-Javadoc)
+//	 * @see com.nightlabs.jdo.search.SearchFilterItem#isConstraint()
+//	 */
+//	@Override
+//	public boolean isConstraint() {
+//		return (nameFilter != null) || (internalFilter != INTERNAL_FILTER_BOTH);
+//	}
+//
+//	/* (non-Javadoc)
+//	 * @see com.nightlabs.jdo.search.SearchFilterItem#appendSubQuery(int, int, java.util.Set, java.lang.StringBuffer, java.lang.StringBuffer, java.lang.StringBuffer, java.util.Map)
+//	 */
+//	@Override
+//	public void appendSubQuery(int itemIndex, Set imports, StringBuffer vars,
+//			StringBuffer filter, StringBuffer params, Map paramMap) {
+//
+//	}
 	
 	public int getInternalFilter() {
 		return internalFilter;
@@ -112,6 +97,24 @@ public class ReportCategorySearchFilterItem extends SearchFilterItem {
 	
 	public void setNameFilter(String nameFilter) {
 		this.nameFilter = nameFilter;
+	}
+
+	@Override
+	public void appendSubQuery(int itemIndex, Set<Class<?>> imports, StringBuffer vars, StringBuffer filter, StringBuffer params,
+			Map<String, Object> paramMap) {
+		filter.append("(");
+		if (nameFilter != null)
+			filter.append("this.name.names.contains()");
+		// TODO: Howto search for name in map ...
+		
+		filter.append(")");
+	}
+	
+	public static final EnumSet<MatchType> SUPPORTED_MATCH_TYPES = EnumSet.of(MatchType.CONTAINS);
+
+	@Override
+	public EnumSet<MatchType> getSupportedMatchTypes() {
+		return SUPPORTED_MATCH_TYPES;
 	}
 
 }

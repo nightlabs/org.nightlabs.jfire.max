294e2521e7346ba576c7a4f8662daa1147f3b2e2
frederik <frederik@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2010-03-30 13:25:24 +0000

    * added logic for ensuring that all fallback files exist in the case the resource to be found exists, as BIRT does not call findResource again (this fixes the bug concerning missing resource files)
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.reporting.birt@17274 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/reporting/birt/platform/ServerResourceLocator.java
+++ src/org/nightlabs/jfire/reporting/birt/platform/ServerResourceLocator.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package org.nightlabs.jfire.reporting.birt.platform;
 
@@ -17,6 +17,7 @@ import org.apache.log4j.Logger;
 import org.eclipse.birt.report.model.api.DefaultResourceLocator;
 import org.eclipse.birt.report.model.api.IResourceLocator;
 import org.eclipse.birt.report.model.api.ModuleHandle;
+import org.nightlabs.jfire.reporting.layout.LocalisationFileName;
 import org.nightlabs.jfire.reporting.layout.ReportLayout;
 import org.nightlabs.jfire.reporting.layout.ReportLayoutLocalisationData;
 import org.nightlabs.jfire.reporting.layout.id.ReportLayoutLocalisationDataID;
@@ -31,7 +32,7 @@ public class ServerResourceLocator extends DefaultResourceLocator implements IRe
 {
 	private static Logger logger = Logger.getLogger(ServerResourceLocator.class);
 	private static ThreadLocal<ReportLayout> currentReportLayout = new ThreadLocal<ReportLayout>();
-	
+
 	public static void setCurrentReportLayout(ReportLayout reportLayout)
 	{
 		currentReportLayout.set(reportLayout);
@@ -43,45 +44,90 @@ public class ServerResourceLocator extends DefaultResourceLocator implements IRe
 	}
 
 	/**
-	 * 
+	 *
 	 */
 	public ServerResourceLocator() {
 	}
 
 	/**
-	 * Tries to find
+	 * Tries to find a resource (normally for localisation).
 	 * <p>
 	 * {@inheritDoc}
 	 * @see org.eclipse.birt.report.model.api.IResourceLocator#findResource(org.eclipse.birt.report.model.api.ModuleHandle, java.lang.String, int)
 	 */
 	@Override
 	public URL findResource(ModuleHandle handle, String fileName, int type) {
-		String locale = ReportLayoutLocalisationData.extractLocale(fileName);
+		LocalisationFileName localisationFileName = new LocalisationFileName(fileName);
+		System.out.println("findResource called...");
+
+		String locale = localisationFileName.getLocale();
 		if (locale == null)
 			locale = "";
 
 		ReportLayout layout = getCurrentReportLayout();
 		if (layout != null) {
-			ReportLayoutLocalisationDataID localisationID = ReportLayoutLocalisationDataID.create(
-					layout.getOrganisationID(),
-					layout.getReportRegistryItemType(),
-					layout.getReportRegistryItemID(),
-					locale
-			);
-			PersistenceManager pm = JDOHelper.getPersistenceManager(layout);
-			if (pm == null)
-				throw new IllegalStateException("Could not obtain PersistenceManager for ReportLayout "+JDOHelper.getObjectId(layout));
-			ReportLayoutLocalisationData localisationData = null;
-			try {
-				localisationData = (ReportLayoutLocalisationData) pm.getObjectById(localisationID);
-				localisationData.getLocale(); // WORKAROUND: Need to access
-			} catch (JDOObjectNotFoundException e) {
-				return null;
+			URL result = findResource(handle, fileName, type, layout, locale);
+
+			// If the resource exists, we make sure that the fallback files exist, too, because BIRT
+			// will search for them without calling this method again.
+			// For example, if the fileName is "messages_de_CH.properties", we make sure
+			// that "messages_de.properties" and "messages.properties" exist, too
+			// (if the data exists in our database, only, of course).
+			//
+			// If the resource does not exist (i.e. result == null), we do nothing, because
+			// BIRT will then call this method again for the next fallback file name.
+			//
+			// Marco.
+			if (result != null) {
+				while (!locale.isEmpty()) {
+					int i = locale.lastIndexOf('_');
+					if (i < 0)
+						locale = "";
+					else
+						locale = locale.substring(0, i);
+
+					String fn = localisationFileName.createFullName(locale);
+					findResource(handle, fn, type, layout, locale);
+				}
+
+				return result;
 			}
+		}
+		return super.findResource(handle, fileName, type);
+	}
+
+	private URL findResource(ModuleHandle handle, String fileName, int type, ReportLayout layout, String locale)
+	{
+		try {
 			File layoutRoot = ReportLayoutRendererUtil.getRenderedLayoutOutputFolder();
 			File outputFile = new File(layoutRoot, fileName);
-			InputStream in = localisationData.createLocalisationDataInputStream();
-			try {
+			// TODO We cannot ensure that the directory is new (and thus empty), because the current API is not clean and an existing directory might be used.
+			// Therefore, we temporarily force overwriting of the resource files. Freddy+Marco.
+			if (false && outputFile.exists()) { // remove the 'false' here, when the cleanup is done!
+				// This code is ALWAYS executed on the server-side with a unique directory for each report-rendering. TODO currently not true :-( Marco.
+				if (logger.isDebugEnabled())
+					logger.debug("findResource: The file already exists, will skip and thus NOT overwrite it: " + outputFile.getAbsolutePath());
+			}
+			else {
+				ReportLayoutLocalisationDataID localisationID = ReportLayoutLocalisationDataID.create(
+						layout.getOrganisationID(),
+						layout.getReportRegistryItemType(),
+						layout.getReportRegistryItemID(),
+						locale
+				);
+				PersistenceManager pm = JDOHelper.getPersistenceManager(layout);
+				if (pm == null)
+					throw new IllegalStateException("Could not obtain PersistenceManager from ReportLayout "+JDOHelper.getObjectId(layout));
+
+				ReportLayoutLocalisationData localisationData = null;
+				try {
+					localisationData = (ReportLayoutLocalisationData) pm.getObjectById(localisationID);
+					localisationData.getLocale(); // WORKAROUND: Need to access
+				} catch (JDOObjectNotFoundException e) {
+					return null;
+				}
+
+				InputStream in = localisationData.createLocalisationDataInputStream();
 				try {
 					FileOutputStream out = new FileOutputStream(outputFile);
 					try {
@@ -92,12 +138,21 @@ public class ServerResourceLocator extends DefaultResourceLocator implements IRe
 				} finally {
 					in.close();
 				}
-				return outputFile.toURL();
-			} catch (IOException e) {
-				logger.error(e);
-				return null;
+
+				if (logger.isDebugEnabled())
+					logger.debug("findResource: Created localisation file: " + outputFile.getAbsolutePath());
 			}
+			// TODO Shouldn't the following be outputFile.toURI().toURL() instead of outputFile.toURL()? The difference is AFAIK the
+			// handling of spaces. The current code keeps spaces and simply adds 'file:' at the beginning, while the correct URL-form
+			// would have %20 instead of spaces. What does BIRT expect?
+			// Note, that this must be tested on Windows, because on Linux, there usually are NO spaces (the temp folder is /tmp).
+			// On Windows, however, the temp folder often is located at "C:\Documents and Settings\....".
+			// After testing both versions on Windows, it should be documented here! If both work, the non-deprecated version (toURI().toURL()) should be used instead.
+			// Freddy+Marco.
+			return outputFile.toURL();
+		} catch (IOException e) {
+			logger.error("findResource: " + e, e);
+			return null;
 		}
-		return super.findResource(handle, fileName, type);
 	}
 }

733520d988cd32b4c92c50c0422c13f3640452c5
frederik <frederik@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2010-04-09 19:59:32 +0000

    * introduced new property set field inheritor * fixed bug concerning wrong sort order of DataBlocks when adding new DataBlocks in GUI * adapted DataFields with respect to DataBlock ID (clone; constructor)
    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireDepartment@17369 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/department/prop/DepartmentDataField.java
+++ src/org/nightlabs/jfire/department/prop/DepartmentDataField.java
@@ -1,24 +1,24 @@
 package org.nightlabs.jfire.department.prop;
 
+import javax.jdo.annotations.FetchGroup;
+import javax.jdo.annotations.FetchGroups;
+import javax.jdo.annotations.IdentityType;
+import javax.jdo.annotations.Inheritance;
+import javax.jdo.annotations.InheritanceStrategy;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+
 import org.nightlabs.jfire.department.Department;
 import org.nightlabs.jfire.prop.DataBlock;
 import org.nightlabs.jfire.prop.DataField;
 import org.nightlabs.jfire.prop.PropertySet;
 import org.nightlabs.jfire.prop.StructField;
 
-import javax.jdo.annotations.Persistent;
-import javax.jdo.annotations.FetchGroups;
-import javax.jdo.annotations.InheritanceStrategy;
-import javax.jdo.annotations.Inheritance;
-import javax.jdo.annotations.PersistenceCapable;
-import javax.jdo.annotations.FetchGroup;
-import javax.jdo.annotations.IdentityType;
-
 /**
- * {@link DataField} that stores a department. 
- * 
+ * {@link DataField} that stores a department.
+ *
  * @author Chairat Kongarayawetchakun - chairat at nightlabs dot de
- * 
+ *
  * @jdo.persistence-capable
  * 		identity-type="application"
  * 		detachable="true"
@@ -61,9 +61,19 @@ public class DepartmentDataField extends DataField
 		this.department = cloneField.department;
 	}
 
+	protected DepartmentDataField(String organisationID, long propertySetID, int dataBlockID, DepartmentDataField cloneField) {
+		super(organisationID, propertySetID, dataBlockID, cloneField);
+		this.department = cloneField.department;
+	}
+
 	@Override
 	public DataField cloneDataField(PropertySet propertySet) {
-		return new DepartmentDataField(propertySet.getOrganisationID(), propertySet.getPropertySetID(), this);
+		return cloneDataField(propertySet, 0);
+	}
+
+	@Override
+	public DataField cloneDataField(PropertySet propertySet, int dataBlockID) {
+		return new DepartmentDataField(propertySet.getOrganisationID(), propertySet.getPropertySetID(), dataBlockID, this);
 	}
 
 	@Override
@@ -80,7 +90,7 @@ public class DepartmentDataField extends DataField
 	public void setData(Object data) {
 		setDepartment((Department) data);
 	}
-	
+
 	public Department getDepartment() {
 		return department;
 	}

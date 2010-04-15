cd487a985b39180ac1e608b85c505cff24e85966
frederik <frederik@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2010-04-15 14:23:30 +0000

    * minor changes
    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireHTMLProp@17407 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/prop/html/HTMLDataField.java
+++ src/org/nightlabs/jfire/prop/html/HTMLDataField.java
@@ -19,6 +19,7 @@ import javax.jdo.annotations.PersistenceModifier;
 import javax.jdo.annotations.Persistent;
 import javax.jdo.annotations.Value;
 
+import org.apache.log4j.Logger;
 import org.nightlabs.htmlcontent.IFCKEditorContent;
 import org.nightlabs.htmlcontent.IFCKEditorContentFile;
 import org.nightlabs.htmlcontent.IFCKEditorContentFileFactory;
@@ -53,6 +54,11 @@ import org.nightlabs.util.NLLocale;
 @Inheritance(strategy=InheritanceStrategy.NEW_TABLE)
 public class HTMLDataField extends DataField
 {
+	/**
+	 *
+	 */
+	private static final Logger LOGGER = Logger.getLogger(HTMLDataField.class);
+
 	/**
 	 * The serial version of this class.
 	 */
@@ -287,12 +293,21 @@ public class HTMLDataField extends DataField
 		}
 	}
 
-	private static class HTMLDataFieldContent {
+	/**
+	 * Helper class wrapping properties of this {@link DataField} instance. It is used for inheritance purposes in the case the contents of {@link HTMLDataField}s are inherited from mother to child.
+	 * @author Frederik Loeser <!-- frederik [AT] nightlabs [DOT] de -->
+	 */
+	private static class HTMLDataFieldContentDescriptor {
 
 		private Map<String, String> texts;
 		private List<IFCKEditorContentFile> files;
 
-		HTMLDataFieldContent(final Map<String, String> texts, final List<IFCKEditorContentFile> files) {
+		/**
+		 * Initialises a new {@link HTMLDataFieldContentDescriptor} instance.
+		 * @param texts
+		 * @param files
+		 */
+		HTMLDataFieldContentDescriptor(final Map<String, String> texts, final List<IFCKEditorContentFile> files) {
 			this.texts = texts;
 			this.files = files;
 		}
@@ -314,27 +329,32 @@ public class HTMLDataField extends DataField
 	/**
 	 * {@inheritDoc}
 	 * <p>
-	 * Returns a new instance of {@link HTMLDataFieldContent} wrapping properties of this data field.
+	 * Returns a new instance of {@link HTMLDataFieldContentDescriptor} wrapping properties of this data field.
 	 */
 	@Override
 	public Object getData() {
 //		return getContent(NLLocale.getDefault().getLanguage());
-		return new HTMLDataFieldContent(getTexts(), getFiles());
+		return new HTMLDataFieldContentDescriptor(getTexts(), getFiles());
 	}
 
 	/**
 	 * {@inheritDoc}
 	 * <p>
-	 * Sets the data of this data field according to the data given. In the case an instance of {@link HTMLDataFieldContent} is
+	 * Sets the data of this data field according to the data given. In the case an instance of {@link HTMLDataFieldContentDescriptor} is
 	 * given data are set according to the values wrapped by this instance.
 	 */
 	@Override
-	public void setData(Object data) {
-		if (data instanceof HTMLDataFieldContent) {
-			HTMLDataFieldContent content = (HTMLDataFieldContent) data;
+	public void setData(final Object data) {
+		if (data instanceof HTMLDataFieldContentDescriptor) {
+			final HTMLDataFieldContentDescriptor content = (HTMLDataFieldContentDescriptor) data;
+			if (LOGGER.isDebugEnabled()) {
+				for (Map.Entry<String, String> entry : content.getTexts().entrySet()) {
+					LOGGER.debug("Key:   " + entry.getKey());
+					LOGGER.debug("Value: " + entry.getValue());
+				}
+			}
 			setTexts(new HashMap<String, String>(content.getTexts()));
 			setFiles(new ArrayList<IFCKEditorContentFile>(content.getFiles()));
-
 		}
 	}
 

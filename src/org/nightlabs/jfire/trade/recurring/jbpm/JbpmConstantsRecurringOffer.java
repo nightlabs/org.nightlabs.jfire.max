8687fffd06d6d3e3b93048dd201507fb96f8a01f
fitas_java <fitas_java@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2010-08-17 13:14:39 +0000

    added the variable node NODE_NAME_RECURRENCE_ACCEPTED in the class JbpmConstantsRecurringOffer
    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireTrade@17940 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/trade/recurring/jbpm/JbpmConstantsRecurringOffer.java
+++ src/org/nightlabs/jfire/trade/recurring/jbpm/JbpmConstantsRecurringOffer.java
@@ -15,10 +15,10 @@ public class JbpmConstantsRecurringOffer {
 	public static class Vendor extends JbpmConstantsOffer.Vendor {
 		public static final String NODE_NAME_RECURRENCE_STARTED =
 			Organisation.DEV_ORGANISATION_ID + ":recurrenceStarted";
-	
+		public static final String NODE_NAME_RECURRENCE_ACCEPTED =
+			Organisation.DEV_ORGANISATION_ID + ":accepted";
 		public static final String NODE_NAME_RECURRENCE_STOPED =
 			Organisation.DEV_ORGANISATION_ID + ":recurrenceStopped";
-		
 		public static final String TRANSITION_NAME_START_RECURRENCE =
 			Organisation.DEV_ORGANISATION_ID + ":startRecurrence";
 		public static final String TRANSITION_NAME_PAUSE_RECURRENCE =
